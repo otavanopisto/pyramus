0dea5c28b9698a7997bc3c641de3e596f98dd45f
Beox <panu.puurtinen@otavanopisto.fi>
2013-07-17 11:07:44 +0300

    **Created getLanguage function

--- rest/src/main/java/fi/pyramus/rest/StudentRESTService.java
+++ rest/src/main/java/fi/pyramus/rest/StudentRESTService.java
@@ -6,12 +6,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
 import fi.pyramus.dao.DAOFactory;
 import fi.pyramus.dao.students.StudentDAO;
 import fi.pyramus.domainmodel.students.Student;
 
-@Path("students")
+@Path("/students")
 public class StudentRESTService {
-  
+  @Path("/languages")
+  @GET
+  public Response getLanguages(){
+    return Response.ok().entity("laama").build();
+  }
   
 }
