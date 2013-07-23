f3b5d79c6da62caa7e9647904649efa92fd7be7c
Beox <panu.puurtinen@otavanopisto.fi>
2013-07-23 12:10:29 +0300

    **Fixed attributes for TranquilityEntityFactory()

--- rest/src/main/java/fi/pyramus/rest/TranquilityBuilderFactoryProvider.java
+++ rest/src/main/java/fi/pyramus/rest/TranquilityBuilderFactoryProvider.java
@@ -3,9 +3,8 @@ package fi.pyramus.rest;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
-import fi.pyramus.rest.tranquil.BaseLookup;
-import fi.pyramus.rest.tranquil.CompactLookup;
-import fi.pyramus.rest.tranquil.CompleteLookup;
+
+import fi.pyramus.rest.tranquil.EntityLookup;
 import fi.tranquil.TranquilityBuilderFactory;
 import fi.tranquil.processing.PropertyAccessor;
 import fi.tranquil.processing.TranquilityEntityFactory;
@@ -14,6 +13,7 @@ public class TranquilityBuilderFactoryProvider {
   @Produces
   @ApplicationScoped
   public TranquilityBuilderFactory produceTranquilityFactory() {
-    return new TranquilityBuilderFactory(new PropertyAccessor(), new TranquilityEntityFactory(new BaseLookup(), new CompactLookup(), new CompleteLookup()));
+    EntityLookup lookup = new EntityLookup();
+    return new TranquilityBuilderFactory(new PropertyAccessor(), new TranquilityEntityFactory(lookup, lookup, lookup));
   }
 }
