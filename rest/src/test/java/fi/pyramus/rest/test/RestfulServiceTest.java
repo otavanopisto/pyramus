e29ff7fc8ce415c2c690eebfd358688d9df495fd
Beox <panu.puurtinen@otavanopisto.fi>
2013-07-31 11:03:51 +0300

    ** Disabled serializeEntity(), using StringEntity with JSON instead in doPostRequest() ** Made doDeleteRequest() ** Made Create, Get, Update and Delete tests for schools and schoolFields

--- rest/src/test/java/fi/pyramus/rest/test/RestfulServiceTest.java
+++ rest/src/test/java/fi/pyramus/rest/test/RestfulServiceTest.java
@@ -9,6 +9,7 @@ import java.net.URISyntaxException;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -30,84 +31,87 @@ import fi.pyramus.rest.TranquilityBuilderFactoryProvider;
 
 public class RestfulServiceTest {
 
-	public RestfulServiceTest() throws URISyntaxException {
-		baseUri = new URI("http://localhost:8080/test/1");
-	}
-	
-	protected static Archive<?> createArchive(Class<?>... testClasses) {
-		MavenDependencyResolver resolver = DependencyResolvers
-				.use(MavenDependencyResolver.class)
-				.loadMetadataFromPom("pom.xml");
-		
-		
-		return ShrinkWrap.create(WebArchive.class, "test.war")
-			.addAsLibraries(getHibernateSearchFiles(resolver))
-			.addPackages(true, getPersistencePackages())
-		  .addClasses(testClasses)
-		  .addClasses(AbstractRESTService.class, TranquilityBuilderFactoryProvider.class, JaxRsActivator.class)
-		  .addClasses(org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver.class, RestfulServiceTest.class)
-		  .addAsLibraries(resolver.artifact("fi.tranquil-model:tranquil-model:1.0.3-SNAPSHOT").resolveAsFiles())
-		  .addAsLibraries(resolver.artifact("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven:1.0.0-beta-7").resolveAsFiles())
-		  .addAsLibraries(resolver.artifact("org.apache.httpcomponents:httpcore:4.1.2").resolveAsFiles())
-		  .addAsLibraries(resolver.artifact("org.apache.httpcomponents:httpclient:4.1.2").resolveAsFiles())
-      .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
-      .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-      .addAsWebInfResource("test-ds.xml");
-	}
-	
-	protected static Package[] getPersistencePackages() {
-		Package domainModelPackage = fi.pyramus.domainmodel.DomainModelExtensionDescriptor.class.getPackage();
-		Package daoPackage = fi.pyramus.dao.GenericDAO.class.getPackage();
-		Package persistenceSearchPackage = fi.pyramus.persistence.search.SearchResult.class.getPackage();
-		Package persistenceUserTypesPackage = fi.pyramus.persistence.usertypes.MonetaryAmount.class.getPackage();
-		Package tranquilPackage = fi.pyramus.rest.tranquil.EntityLookup.class.getPackage();
-		
-		return new Package[] {
-		  domainModelPackage, daoPackage, persistenceSearchPackage, persistenceUserTypesPackage, tranquilPackage
-	  };
-	}
-	
-	protected static File[] getHibernateSearchFiles(MavenDependencyResolver resolver) {
-		File[] hibernateSearchFiles = resolver.artifact("org.hibernate:hibernate-search:4.1.0.CR3")
-				.exclusions("org.hibernate:hibernate-core:*")
-				.resolveAsFiles();
-		File[] luceneQueriesFiles = resolver.artifact("org.apache.lucene:lucene-queries:3.5.0").resolveAsFiles();
-		
-		return ArrayUtils.addAll(hibernateSearchFiles, luceneQueriesFiles);
-	}
+  public RestfulServiceTest() throws URISyntaxException {
+    baseUri = new URI("http://localhost:8080/test/1");
+  }
 
-	protected static void log(String message) {
-		System.out.println(message);
-	}
-	
-	protected String serializeEntity(Object entity) throws JsonGenerationException, JsonMappingException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.writeValueAsString(entity); 
-	}
-	
-	protected <T> T unserializeEntity(Class<? extends T> clazz, String data) throws JsonParseException, JsonMappingException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		StringReader stringReader = new StringReader(data);
-		try {
-			return mapper.readValue(stringReader, clazz);
-		} finally {
-			stringReader.close();
-		}
-	}
-		
-	protected HttpResponse doPostRequest(String path, Object entity) throws ClientProtocolException, IOException {
-		DefaultHttpClient httpClient = new DefaultHttpClient();
-		HttpPost request = new HttpPost(baseUri + path);
-		String serialized = serializeEntity(entity);
-		request.setEntity(new StringEntity(serialized, "application/json", "UTF-8"));
-		return httpClient.execute(request);
-	}
-	
-	protected HttpResponse doGetRequest(String path) throws ClientProtocolException, IOException {
-		DefaultHttpClient httpClient = new DefaultHttpClient();
-		HttpGet request = new HttpGet(baseUri + path);
-		return httpClient.execute(request);
-	}
-	
-	private URI baseUri;
+  protected static Archive<?> createArchive(Class<?>... testClasses) {
+    MavenDependencyResolver resolver = DependencyResolvers
+        .use(MavenDependencyResolver.class)
+        .loadMetadataFromPom("pom.xml");
+
+    return ShrinkWrap.create(WebArchive.class, "test.war")
+        .addAsLibraries(getHibernateSearchFiles(resolver))
+        .addPackages(true, getPersistencePackages())
+        .addClasses(testClasses)
+        .addClasses(AbstractRESTService.class, TranquilityBuilderFactoryProvider.class, JaxRsActivator.class)
+        .addClasses(org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver.class, RestfulServiceTest.class)
+        .addAsLibraries(resolver.artifact("fi.tranquil-model:tranquil-model:1.0.3-SNAPSHOT").resolveAsFiles())
+        .addAsLibraries(resolver.artifact("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven:1.0.0-beta-7").resolveAsFiles())
+        .addAsLibraries(resolver.artifact("org.apache.httpcomponents:httpcore:4.1.2").resolveAsFiles())
+        .addAsLibraries(resolver.artifact("org.apache.httpcomponents:httpclient:4.1.2").resolveAsFiles())
+        .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+        .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+        .addAsWebInfResource("test-ds.xml");
+  }
+
+  protected static Package[] getPersistencePackages() {
+    Package domainModelPackage = fi.pyramus.domainmodel.DomainModelExtensionDescriptor.class.getPackage();
+    Package daoPackage = fi.pyramus.dao.GenericDAO.class.getPackage();
+    Package persistenceSearchPackage = fi.pyramus.persistence.search.SearchResult.class.getPackage();
+    Package persistenceUserTypesPackage = fi.pyramus.persistence.usertypes.MonetaryAmount.class.getPackage();
+    Package tranquilPackage = fi.pyramus.rest.tranquil.EntityLookup.class.getPackage();
+
+    return new Package[] { domainModelPackage, daoPackage, persistenceSearchPackage, persistenceUserTypesPackage, tranquilPackage };
+  }
+
+  protected static File[] getHibernateSearchFiles(MavenDependencyResolver resolver) {
+    File[] hibernateSearchFiles = resolver.artifact("org.hibernate:hibernate-search:4.1.0.CR3").exclusions("org.hibernate:hibernate-core:*").resolveAsFiles();
+    File[] luceneQueriesFiles = resolver.artifact("org.apache.lucene:lucene-queries:3.5.0").resolveAsFiles();
+
+    return ArrayUtils.addAll(hibernateSearchFiles, luceneQueriesFiles);
+  }
+
+  protected static void log(String message) {
+    System.out.println(message);
+  }
+
+  protected String serializeEntity(Object entity) throws JsonGenerationException, JsonMappingException, IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    return mapper.writeValueAsString(entity);
+  }
+
+  protected <T> T unserializeEntity(Class<? extends T> clazz, String data) throws JsonParseException, JsonMappingException, IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    StringReader stringReader = new StringReader(data);
+    try {
+      return mapper.readValue(stringReader, clazz);
+    } finally {
+      stringReader.close();
+    }
+  }
+
+  protected HttpResponse doPostRequest(String path, StringEntity entity) throws ClientProtocolException, IOException {
+    DefaultHttpClient httpClient = new DefaultHttpClient();
+    HttpPost request = new HttpPost(baseUri + path);
+    // String serialized = serializeEntity(entity);
+    // request.setEntity(new StringEntity(serialized, "application/json", "UTF-8"));
+    request.addHeader("content-type", "application/json");
+    request.setEntity(entity);
+    return httpClient.execute(request);
+  }
+
+  protected HttpResponse doGetRequest(String path) throws ClientProtocolException, IOException {
+    DefaultHttpClient httpClient = new DefaultHttpClient();
+    HttpGet request = new HttpGet(baseUri + path);
+    return httpClient.execute(request);
+  }
+
+  protected HttpResponse doDeleteRequest(String path) throws ClientProtocolException, IOException {
+    DefaultHttpClient httpClient = new DefaultHttpClient();
+    HttpDelete request = new HttpDelete(baseUri + path);
+    return httpClient.execute(request);
+  }
+
+  private URI baseUri;
 }
--- rest/src/test/java/fi/pyramus/rest/test/SchoolRESTServiceTest.java
+++ rest/src/test/java/fi/pyramus/rest/test/SchoolRESTServiceTest.java
@@ -12,6 +12,7 @@ import javax.inject.Inject;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OverProtocol;
@@ -24,139 +25,269 @@ import fi.pyramus.domainmodel.base.School;
 import fi.pyramus.rest.SchoolRESTService;
 import fi.pyramus.rest.controller.SchoolController;
 import fi.pyramus.rest.tranquil.base.SchoolEntity;
+import fi.pyramus.rest.tranquil.base.SchoolFieldEntity;
 
 @RunWith(Arquillian.class)
 public class SchoolRESTServiceTest extends RestfulServiceTest {
-	
-	@Inject
-	private SchoolController schoolController;
 
-	@Inject
-	private InitialSchoolDataDescriptor initialSchoolDataDescriptor;
+  @Inject
+  private SchoolController schoolController;
 
-	public SchoolRESTServiceTest() throws URISyntaxException {
-		super();
-	}
-	
-	@Deployment
-	@OverProtocol("Servlet 3.0")
-  public static Archive<?> createTestArchive() {
-		Archive<?> archive = createArchive(
- 		  InitialSchoolDataDescriptor.class,
-			SchoolController.class,
-		  SchoolRESTService.class
-		);
-		
-		return archive;
-	}	
-	
-	@Test
-	public void testSanity() {
-		assertNotNull(schoolController);
-		School school = schoolController.getSchoolById(initialSchoolDataDescriptor.getSchoolId());
-		assertNotNull(school);
-		assertEquals(initialSchoolDataDescriptor.getSchoolId(), school.getId());
+  @Inject
+  private InitialSchoolDataDescriptor initialSchoolDataDescriptor;
+
+  public SchoolRESTServiceTest() throws URISyntaxException {
+    super();
   }
-	
 
-	@Test
-	public void testCreateSchool() {
-		fail("Not yet implemented");
-	}
+  @Deployment
+  @OverProtocol("Servlet 3.0")
+  public static Archive<?> createTestArchive() {
+    Archive<?> archive = createArchive(InitialSchoolDataDescriptor.class, SchoolController.class, SchoolRESTService.class);
 
-	@Test
-	public void testCreateSchoolField() {
-		fail("Not yet implemented");
-	}
+    return archive;
+  }
 
-	@Test
-	public void testGetSchools() throws ClientProtocolException, IOException {
-		HttpResponse response = doGetRequest("/schools/schools");
-		
-		assertEquals(200, response.getStatusLine().getStatusCode());
-		
-		HttpEntity entity = response.getEntity();
-		try {
-  		assertNotNull(entity);
-  		assertEquals("application/json", entity.getContentType().getValue());
-	  	SchoolEntity[] schoolEntities = unserializeEntity(SchoolEntity[].class, EntityUtils.toString(entity));
-  		assertNotNull(schoolEntities);
-  		assertEquals(1, schoolEntities.length);
-  		assertEquals(initialSchoolDataDescriptor.getSchoolId(), schoolEntities[0].getId());
-  		// TODO: Name, Code tests
-		} finally {
-			EntityUtils.consume(entity);
-		}
-	}
+  @Test
+  public void testSanity() {
+    assertNotNull(schoolController);
+    School school = schoolController.findSchoolById(initialSchoolDataDescriptor.getSchoolId());
+    assertNotNull(school);
+    assertEquals(initialSchoolDataDescriptor.getSchoolId(), school.getId());
+  }
 
-	@Test
-	public void testGetSchoolById() throws ClientProtocolException, IOException {
-		HttpResponse response = doGetRequest("/schools/schools/" + initialSchoolDataDescriptor.getSchoolId());
-		
-		assertEquals(200, response.getStatusLine().getStatusCode());
-		
-		HttpEntity entity = response.getEntity();
-		try {
-  		assertNotNull(entity);
-  		assertEquals("application/json", entity.getContentType().getValue());
-	  	SchoolEntity schoolEntity = unserializeEntity(SchoolEntity.class, EntityUtils.toString(entity));
-  		assertNotNull(schoolEntity);
-  		assertEquals(initialSchoolDataDescriptor.getSchoolId(), schoolEntity.getId());
-  		assertEquals(initialSchoolDataDescriptor.getSchoolName(), schoolEntity.getName());
-  		assertEquals(initialSchoolDataDescriptor.getSchoolCode(), schoolEntity.getCode());
-		} finally {
-			EntityUtils.consume(entity);
-		}
-	}
+  @Test
+  public void testCreateSchool() throws ClientProtocolException, IOException {
+    StringEntity str = new StringEntity("{\"field_id\":1,\"code\":\"TAMK\",\"name\":\"Tampereen ammattikorkeakoulu\"}");
 
-	@Test
-	public void testGetSchoolVariablesBySchool() {
-		fail("Not yet implemented");
-	}
+    HttpResponse response = doPostRequest("/schools/schools/", str);
 
-	@Test
-	public void testGetSchoolFields() {
-		fail("Not yet implemented");
-	}
+    assertEquals(200, response.getStatusLine().getStatusCode());
+    HttpEntity entity = response.getEntity();
+    try {
+      assertNotNull(entity);
+      assertEquals("application/json", entity.getContentType().getValue());
+      SchoolEntity schoolEntity = unserializeEntity(SchoolEntity.class, EntityUtils.toString(entity));
+      assertNotNull(schoolEntity);
+      assertEquals("Tampereen ammattikorkeakoulu", schoolEntity.getName());
+      assertEquals("TAMK", schoolEntity.getCode());
+      assertEquals((Long) 1l, schoolEntity.getField_id());
+    } finally {
+      EntityUtils.consume(entity);
+    }
+  }
 
-	@Test
-	public void testGetSchoolFieldByID() {
-		fail("Not yet implemented");
-	}
+  @Test
+  public void testCreateSchoolField() throws ClientProtocolException, IOException {
+    StringEntity str = new StringEntity("{\"name\":\"Polytechnic\"}");
 
-	@Test
-	public void testGetVariables() {
-		fail("Not yet implemented");
-	}
+    HttpResponse response = doPostRequest("/schools/schoolFields/", str);
 
-	@Test
-	public void testGetVariablesdByID() {
-		fail("Not yet implemented");
-	}
+    assertEquals(200, response.getStatusLine().getStatusCode());
+    HttpEntity entity = response.getEntity();
+    try {
+      assertNotNull(entity);
+      assertEquals("application/json", entity.getContentType().getValue());
+      SchoolFieldEntity schoolFieldEntity = unserializeEntity(SchoolFieldEntity.class, EntityUtils.toString(entity));
+      assertNotNull(schoolFieldEntity);
+      assertEquals("Polytechnic", schoolFieldEntity.getName());
+    } finally {
+      EntityUtils.consume(entity);
+    }
+  }
 
-	@Test
-	public void testUpdateSchool() {
-		fail("Not yet implemented");
-	}
+  @Test
+  public void testFindSchools() throws ClientProtocolException, IOException {
+    HttpResponse response = doGetRequest("/schools/schools");
 
-	@Test
-	public void testUpdateSchoolField() {
-		fail("Not yet implemented");
-	}
+    assertEquals(200, response.getStatusLine().getStatusCode());
 
-	@Test
-	public void testUpdateSchoolVariable() {
-		fail("Not yet implemented");
-	}
+    HttpEntity entity = response.getEntity();
+    try {
+      assertNotNull(entity);
+      assertEquals("application/json", entity.getContentType().getValue());
+      SchoolEntity[] schoolEntities = unserializeEntity(SchoolEntity[].class, EntityUtils.toString(entity));
+      assertNotNull(schoolEntities);
+      assertEquals(2, schoolEntities.length);
+      assertEquals(initialSchoolDataDescriptor.getSchoolId(), schoolEntities[0].getId());
+      assertEquals(initialSchoolDataDescriptor.getSchoolName(), schoolEntities[0].getName());
+      assertEquals(initialSchoolDataDescriptor.getSchoolCode(), schoolEntities[0].getCode());
+      assertEquals(initialSchoolDataDescriptor.getSchoolFieldId(), schoolEntities[0].getField_id());
+    } finally {
+      EntityUtils.consume(entity);
+    }
+  }
 
-	@Test
-	public void testArchiveSchool() {
-		fail("Not yet implemented");
-	}
+  @Test
+  public void testFindSchoolById() throws ClientProtocolException, IOException {
+    HttpResponse response = doGetRequest("/schools/schools/" + initialSchoolDataDescriptor.getSchoolId());
 
-	@Test
-	public void testArchiveSchoolField() {
-		fail("Not yet implemented");
-	}
+    assertEquals(200, response.getStatusLine().getStatusCode());
+
+    HttpEntity entity = response.getEntity();
+    try {
+      assertNotNull(entity);
+      assertEquals("application/json", entity.getContentType().getValue());
+      SchoolEntity schoolEntity = unserializeEntity(SchoolEntity.class, EntityUtils.toString(entity));
+      assertNotNull(schoolEntity);
+      assertEquals(initialSchoolDataDescriptor.getSchoolId(), schoolEntity.getId());
+      assertEquals(initialSchoolDataDescriptor.getSchoolName(), schoolEntity.getName());
+      assertEquals(initialSchoolDataDescriptor.getSchoolCode(), schoolEntity.getCode());
+      assertEquals(initialSchoolDataDescriptor.getSchoolFieldId(), schoolEntity.getField_id());
+    } finally {
+      EntityUtils.consume(entity);
+    }
+  }
+
+  @Test
+  public void testFindSchoolFields() throws ClientProtocolException, IOException {
+    HttpResponse response = doGetRequest("/schools/schoolFields");
+
+    assertEquals(200, response.getStatusLine().getStatusCode());
+
+    HttpEntity entity = response.getEntity();
+    try {
+      assertNotNull(entity);
+      assertEquals("application/json", entity.getContentType().getValue());
+      SchoolFieldEntity[] schoolFieldEntities = unserializeEntity(SchoolFieldEntity[].class, EntityUtils.toString(entity));
+      assertNotNull(schoolFieldEntities);
+      assertEquals(2, schoolFieldEntities.length);
+      assertEquals(initialSchoolDataDescriptor.getSchoolFieldId(), schoolFieldEntities[0].getId());
+      assertEquals(initialSchoolDataDescriptor.getSchoolFieldName(), schoolFieldEntities[0].getName());
+      assertEquals((Long) 2l, schoolFieldEntities[1].getId());
+      assertEquals("Polytechnic", schoolFieldEntities[1].getName());
+    } finally {
+      EntityUtils.consume(entity);
+    }
+  }
+
+  @Test
+  public void testFindSchoolFieldByID() throws ClientProtocolException, IOException {
+    HttpResponse response = doGetRequest("/schools/schoolFields/" + initialSchoolDataDescriptor.getSchoolFieldId());
+
+    assertEquals(200, response.getStatusLine().getStatusCode());
+
+    HttpEntity entity = response.getEntity();
+    try {
+      assertNotNull(entity);
+      assertEquals("application/json", entity.getContentType().getValue());
+      SchoolFieldEntity schoolFieldEntity = unserializeEntity(SchoolFieldEntity.class, EntityUtils.toString(entity));
+      assertNotNull(schoolFieldEntity);
+      assertEquals(initialSchoolDataDescriptor.getSchoolFieldId(), schoolFieldEntity.getId());
+      assertEquals(initialSchoolDataDescriptor.getSchoolFieldName(), schoolFieldEntity.getName());
+    } finally {
+      EntityUtils.consume(entity);
+    }
+  }
+
+  @Test
+  public void testFindSchoolVariables() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindVariablesByID() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindSchoolVariablesBySchool() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testUpdateSchool() throws ClientProtocolException, IOException {
+
+    String path = "/schools/schools/1";
+
+    StringEntity str = new StringEntity("{\"field_id\":2,\"code\":\"SUST\",\"name\":\"Shahjalal University of Science and Technology\"}");
+
+    HttpResponse response = doPostRequest(path, str);
+
+    assertEquals(200, response.getStatusLine().getStatusCode());
+
+    HttpEntity entity = response.getEntity();
+    try {
+      assertNotNull(entity);
+      assertEquals("application/json", entity.getContentType().getValue());
+      SchoolEntity schoolEntity = unserializeEntity(SchoolEntity.class, EntityUtils.toString(entity));
+      assertNotNull(schoolEntity);
+      assertEquals(initialSchoolDataDescriptor.getSchoolId(), schoolEntity.getId());
+      assertEquals("Shahjalal University of Science and Technology", schoolEntity.getName());
+      assertEquals("SUST", schoolEntity.getCode());
+      assertEquals((Long) 2l, schoolEntity.getField_id());
+    } finally {
+      EntityUtils.consume(entity);
+    }
+  }
+
+  @Test
+  public void testUpdateSchoolField() throws ClientProtocolException, IOException {
+    String path = "/schools/schoolFields/1";
+
+    StringEntity str = new StringEntity("{\"name\":\"College\"}");
+
+    HttpResponse response = doPostRequest(path, str);
+
+    assertEquals(200, response.getStatusLine().getStatusCode());
+    HttpEntity entity = response.getEntity();
+    try {
+      assertNotNull(entity);
+      assertEquals("application/json", entity.getContentType().getValue());
+      SchoolFieldEntity schoolFieldEntity = unserializeEntity(SchoolFieldEntity.class, EntityUtils.toString(entity));
+      assertNotNull(schoolFieldEntity);
+      assertEquals(initialSchoolDataDescriptor.getSchoolFieldId(), schoolFieldEntity.getId());
+      assertEquals("College", schoolFieldEntity.getName());
+    } finally {
+      EntityUtils.consume(entity);
+    }
+  }
+
+  @Test
+  public void testUpdateSchoolVariable() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testArchiveSchool() throws ClientProtocolException, IOException {
+    String path = "/schools/schools/2";
+
+    HttpResponse response = doDeleteRequest(path);
+
+    assertEquals(200, response.getStatusLine().getStatusCode());
+
+    HttpEntity entity = response.getEntity();
+    try {
+      assertNotNull(entity);
+      assertEquals("application/json", entity.getContentType().getValue());
+      SchoolEntity schoolEntity = unserializeEntity(SchoolEntity.class, EntityUtils.toString(entity));
+      assertNotNull(schoolEntity);
+      assertEquals((Long) 2l, schoolEntity.getId());
+      assertEquals(true, schoolEntity.getArchived());
+    } finally {
+      EntityUtils.consume(entity);
+    }
+  }
+
+  @Test
+  public void testArchiveSchoolField() throws ClientProtocolException, IOException {
+    String path = "/schools/schoolFields/2";
+
+    HttpResponse response = doDeleteRequest(path);
+
+    assertEquals(200, response.getStatusLine().getStatusCode());
+
+    HttpEntity entity = response.getEntity();
+    try {
+      assertNotNull(entity);
+      assertEquals("application/json", entity.getContentType().getValue());
+      SchoolFieldEntity schoolFieldEntity = unserializeEntity(SchoolFieldEntity.class, EntityUtils.toString(entity));
+      assertNotNull(schoolFieldEntity);
+      assertEquals((Long) 2l, schoolFieldEntity.getId());
+      assertEquals(true, schoolFieldEntity.getArchived());
+    } finally {
+      EntityUtils.consume(entity);
+    }
+  }
 
 }
