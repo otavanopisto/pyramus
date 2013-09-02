f8a8ffdb820d7aaa8dac212775a8498d29dc3e45
Beox <panu.puurtinen@otavanopisto.fi>
2013-09-02 13:07:29 +0300

    ** Added create, find and update functions for students/abstractStudents

--- rest/src/main/java/fi/pyramus/rest/StudentRESTService.java
+++ rest/src/main/java/fi/pyramus/rest/StudentRESTService.java
@@ -1,23 +1,113 @@
 package fi.pyramus.rest;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-import fi.pyramus.dao.DAOFactory;
-import fi.pyramus.dao.students.StudentDAO;
-import fi.pyramus.domainmodel.students.Student;
+import org.apache.commons.lang3.StringUtils;
+
+import fi.pyramus.domainmodel.students.AbstractStudent;
+import fi.pyramus.domainmodel.students.Sex;
+import fi.pyramus.rest.controller.AbstractStudentController;
+import fi.pyramus.rest.tranquil.students.AbstractStudentEntity;
+import fi.tranquil.TranquilityBuilderFactory;
 
 @Path("/students")
-public class StudentRESTService {
-  @Path("/languages")
-  @GET
-  public Response getLanguages(){
-    return Response.ok().entity("laama").build();
+@Produces("application/json")
+@Consumes("application/json")
+@Stateful
+@RequestScoped
+public class StudentRESTService extends AbstractRESTService {
+  @Inject
+  private TranquilityBuilderFactory tranquilityBuilderFactory;
+  @Inject
+  AbstractStudentController abstractStudentController;
+  
+  @Path("/abstractStudents")
+  @POST
+  public Response createAbstractStudent(AbstractStudentEntity abstractStudentEntity) {
+    Date birthday = abstractStudentEntity.getBirthday();
+    String socialSecurityNumber = abstractStudentEntity.getSocialSecurityNumber();
+    Sex sex = abstractStudentEntity.getSex();
+    String basicInfo = abstractStudentEntity.getBasicInfo();
+    boolean secureInfo = abstractStudentEntity.getSecureInfo();
+    
+    if (birthday != null && !StringUtils.isBlank(socialSecurityNumber) && sex != null && !StringUtils.isBlank(basicInfo)) {
+      return Response.ok()
+          .entity(tranqualise(abstractStudentController.createAbstractStudent(birthday, socialSecurityNumber, sex, basicInfo, secureInfo)))
+          .build();
+    } else  {
+      return Response.status(500).build();
+    }
   }
   
+  @Path("/abstractStudents")
+  @GET
+  public Response findAbstractStudents(@DefaultValue("false") @QueryParam("filterArchived") boolean filterArchived) {
+    List<AbstractStudent> abstractStudents;
+    if (filterArchived) {
+      abstractStudents = abstractStudentController.findUnarchivedAbstractStudents();
+    } else {
+      abstractStudents = abstractStudentController.findAbstractStudents();
+    }
+    if (!abstractStudents.isEmpty()) {
+      return Response.ok()
+          .entity(tranqualise(abstractStudents))
+          .build();
+    } else {
+      return Response.status(Status.NOT_FOUND).build();
+    }
+  }
+  
+  @Path("/abstractStudents/{ID:[0-9]*}")
+  @GET
+  public Response findAbstractStudentById(@PathParam("ID") Long id) {
+    AbstractStudent abstractStudent = abstractStudentController.findAbstractStudentById(id);
+    if (abstractStudent != null) {
+      return Response.ok()
+          .entity(tranqualise(abstractStudent))
+          .build();
+    } else {
+      return Response.status(Status.NOT_FOUND).build();
+    }
+  }
+  
+  @Path("/abstractStudents/{ID:[0-9]*}")
+  @PUT
+  public Response updateAbstractStudent(@PathParam("ID") Long id, AbstractStudentEntity abstractStudentEntity) {
+    AbstractStudent abstractStudent = abstractStudentController.findAbstractStudentById(id);
+    Date birthday = abstractStudentEntity.getBirthday();
+    String socialSecurityNumber = abstractStudentEntity.getSocialSecurityNumber();
+    Sex sex = abstractStudentEntity.getSex();
+    String basicInfo = abstractStudentEntity.getBasicInfo();
+    boolean secureInfo = abstractStudentEntity.getSecureInfo();
+    
+    if (abstractStudent != null &&birthday != null && !StringUtils.isBlank(socialSecurityNumber) && sex != null && !StringUtils.isBlank(basicInfo)) {
+      return Response.ok()
+          .entity(tranqualise(abstractStudentController.updateAbstractStudent(abstractStudent, birthday, socialSecurityNumber, sex, basicInfo, secureInfo)))
+          .build();
+    } else  {
+      return Response.status(500).build();
+    }
+  }
+  
+  @Override
+  protected TranquilityBuilderFactory getTranquilityBuilderFactory() {
+    return tranquilityBuilderFactory;
+  }
+
 }
