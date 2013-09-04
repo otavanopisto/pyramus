325933a75e70ef3f6e21451136bb7a41eff13cb8
Beox <panu.puurtinen@otavanopisto.fi>
2013-09-04 14:04:29 +0300

    ** Added create and update functions

--- persistence/src/main/java/fi/pyramus/dao/students/StudentActivityTypeDAO.java
+++ persistence/src/main/java/fi/pyramus/dao/students/StudentActivityTypeDAO.java
@@ -1,11 +1,29 @@
 package fi.pyramus.dao.students;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
 
 import fi.pyramus.dao.PyramusEntityDAO;
 import fi.pyramus.domainmodel.students.StudentActivityType;
 
 @Stateless
 public class StudentActivityTypeDAO extends PyramusEntityDAO<StudentActivityType> {
-
+ 
+  public StudentActivityType create(String name) {
+    EntityManager entityManager = getEntityManager();
+    
+    StudentActivityType studentActivityType = new StudentActivityType();
+    studentActivityType.setName(name);
+    
+    entityManager.persist(studentActivityType);
+    return studentActivityType;
+  }
+  
+  public StudentActivityType update(StudentActivityType studentActivityType, String name) {
+    EntityManager entityManager = getEntityManager();
+    
+    studentActivityType.setName(name);
+    entityManager.persist(studentActivityType);
+    return studentActivityType;
+  }
 }
