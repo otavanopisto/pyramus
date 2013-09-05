3d303290e6988b5f6bdcedba94929a1b0459bb55
Beox <panu.puurtinen@otavanopisto.fi>
2013-09-05 09:49:41 +0300

    ** Added create and update functions

--- persistence/src/main/java/fi/pyramus/dao/students/StudentEducationalLevelDAO.java
+++ persistence/src/main/java/fi/pyramus/dao/students/StudentEducationalLevelDAO.java
@@ -1,11 +1,30 @@
 package fi.pyramus.dao.students;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
 
 import fi.pyramus.dao.PyramusEntityDAO;
 import fi.pyramus.domainmodel.students.StudentEducationalLevel;
 
 @Stateless
 public class StudentEducationalLevelDAO extends PyramusEntityDAO<StudentEducationalLevel> {
-
+ 
+  public StudentEducationalLevel create(String name) {
+    EntityManager entityManager = getEntityManager();
+    
+    StudentEducationalLevel studentEducationalLevel = new StudentEducationalLevel();
+    studentEducationalLevel.setName(name);
+    
+    entityManager.persist(studentEducationalLevel);
+    return studentEducationalLevel;
+  }
+  
+  public StudentEducationalLevel update(StudentEducationalLevel studentEducationalLevel, String name) {
+    EntityManager entityManager = getEntityManager();
+    
+    studentEducationalLevel.setName(name);
+    
+    entityManager.persist(studentEducationalLevel);
+    return studentEducationalLevel;
+  }
 }
