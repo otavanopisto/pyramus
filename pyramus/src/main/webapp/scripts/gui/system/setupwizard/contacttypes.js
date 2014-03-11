41ca7507810e05ff657d07fa88421266365de15e
ilmoeuro <ilmo.euro@gmail.com>
2014-03-11 12:55:08 +0200

    ** Contact types setup wizard view

---
+++ pyramus/src/main/webapp/scripts/gui/system/setupwizard/contacttypes.js
@@ -0,0 +1,36 @@
+function addContactTypesTableRow() {
+  getIxTableById('contactTypesTable').addRow([ '', '']);
+  $('noContactTypesAddedMessageContainer').setStyle({
+    display : 'none'
+  });
+}
+
+function onLoad(event) {
+  new IxTable($('contactTypesTable'), {
+    id : "contactTypesTable",
+    columns : [{
+      header : getLocale().getText("system.setupwizard.nationalities.nationalitiesTableNameHeader"),
+      left : 8,
+      right: 48,
+      dataType : 'text',
+      paramName : 'name',
+      required : true,
+      editable: true
+    }, {
+      right : 8,
+      width : 30,
+      dataType : 'button',
+      imgsrc : GLOBAL_contextPath + '/gfx/list-remove.png',
+      tooltip : getLocale().getText("system.setupwizard.nationalities.nationalitiesTableRemoveTooltip"),
+      onclick : function(event) {
+        event.tableComponent.deleteRow(event.row);
+        if (event.tableComponent.getRowCount() == 0) {
+          $('noContactTypesAddedMessageContainer').setStyle({
+            display : ''
+          });
+        }
+      },
+      paramName : 'removeButton'
+    }]
+  });
+}
