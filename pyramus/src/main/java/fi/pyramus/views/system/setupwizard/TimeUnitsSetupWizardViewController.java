149717caefed6e88fd0b86b2a0c1b6f81817acd1
Antti Leppä <antti.leppa@foyt.fi>
2014-03-13 11:29:09 +0200

    ** Removed support for modifying timeunits from setup wizard

--- pyramus/src/main/java/fi/pyramus/views/system/setupwizard/TimeUnitsSetupWizardViewController.java
+++ pyramus/src/main/java/fi/pyramus/views/system/setupwizard/TimeUnitsSetupWizardViewController.java
@@ -1,23 +1,10 @@
 package fi.pyramus.views.system.setupwizard;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
-
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-import fi.internetix.smvc.SmvcRuntimeException;
 import fi.internetix.smvc.controllers.PageRequestContext;
 import fi.pyramus.dao.DAOFactory;
-import fi.pyramus.dao.base.AcademicTermDAO;
 import fi.pyramus.dao.base.DefaultsDAO;
 import fi.pyramus.dao.base.EducationalTimeUnitDAO;
-import fi.pyramus.domainmodel.base.AcademicTerm;
 import fi.pyramus.domainmodel.base.EducationalTimeUnit;
-import fi.pyramus.framework.PyramusStatusCode;
-import fi.pyramus.util.JSONArrayExtractor;
 
 public class TimeUnitsSetupWizardViewController extends SetupWizardController {
 
@@ -27,41 +14,6 @@ public class TimeUnitsSetupWizardViewController extends SetupWizardController {
 
   @Override
   public void setup(PageRequestContext requestContext) throws SetupWizardException {
-    EducationalTimeUnitDAO educationalTimeUnitDAO = DAOFactory.getInstance().getEducationalTimeUnitDAO();
-    DefaultsDAO defaultsDAO = DAOFactory.getInstance().getDefaultsDAO();
-    
-    final EducationalTimeUnit baseTimeUnit = defaultsDAO.getDefaults().getBaseTimeUnit();
-    List<EducationalTimeUnit> timeUnits = new ArrayList<EducationalTimeUnit>(educationalTimeUnitDAO.listUnarchived());
-    
-
-    Collections.sort(timeUnits, new Comparator<EducationalTimeUnit>() {
-      @Override
-      public int compare(EducationalTimeUnit o1, EducationalTimeUnit o2) {
-        Double units1 = o1.getBaseUnits();
-        Double units2 = o2.getBaseUnits();
-
-        if (units1 == units2)
-          return 0;
-        
-        if (o1.equals(baseTimeUnit))
-          return -1;
-        if (o2.equals(baseTimeUnit))
-          return 1;
-        
-        return units1 > units2 ? 1 : -1;
-      }
-    });
-    
-    String jsonTimeUnits = new JSONArrayExtractor("id", "baseUnits", "name").extractString(timeUnits);
-    JSONObject joBaseTimeUnit = new JSONObject();
-    if (baseTimeUnit != null) {
-      joBaseTimeUnit.put("id", baseTimeUnit.getId());
-    } else {
-      joBaseTimeUnit.put("id", -1);
-    }
-    
-    this.setJsDataVariable(requestContext, "timeUnits", jsonTimeUnits);
-    this.setJsDataVariable(requestContext, "baseTimeUnit", joBaseTimeUnit.toString());
   }
 
   @Override
@@ -73,11 +25,7 @@ public class TimeUnitsSetupWizardViewController extends SetupWizardController {
 
     int rowCount = requestContext.getInteger("timeUnitsTable.rowCount").intValue();
     for (int i = 0; i < rowCount; i++) {
-      EducationalTimeUnit timeUnit;
-      
       String colPrefix = "timeUnitsTable." + i;
-      Long timeUnitId = requestContext.getLong(colPrefix + ".timeUnitId");
-      
       Boolean baseUnit = "1".equals(requestContext.getString(colPrefix + ".baseUnit"));
       Double baseUnits = requestContext.getDouble(colPrefix + ".baseUnits");
       String name = requestContext.getRequest().getParameter(colPrefix + ".name");
@@ -85,21 +33,23 @@ public class TimeUnitsSetupWizardViewController extends SetupWizardController {
       if (baseUnit) {
         baseUnits = new Double(1);
       }
-        
-      if(timeUnitId == null){
-        throw new SetupWizardException("Initial course participation is not defined");
-      }
 
-      timeUnit = educationalTimeUnitDAO.create(baseUnits, name); 
- 
+      EducationalTimeUnit timeUnit = educationalTimeUnitDAO.create(baseUnits, name); 
+      
       if (baseUnit) {
-        if (baseTimeUnit != null)
-          throw new SmvcRuntimeException(PyramusStatusCode.UNDEFINED, "Two or more baseTimeUnits defined");
-          
-        baseTimeUnit = timeUnit;
+        if (baseTimeUnit != null) {
+          throw new SetupWizardException("Two or more base time units defined");
+        } else {
+          baseTimeUnit = timeUnit; 
+        }
       }
     }
-
+    
+    if (baseTimeUnit != null) {
+      defaultsDAO.updateDefaultBaseTimeUnit(baseTimeUnit);
+    } else {
+      throw new SetupWizardException("Base time unit is not defined");    
+    }
   }
 
   @Override
--- pyramus/src/main/webapp/scripts/gui/system/setupwizard/timeunits.js
+++ pyramus/src/main/webapp/scripts/gui/system/setupwizard/timeunits.js
@@ -1,13 +1,7 @@
-var timeUnits = JSDATA["timeUnits"].evalJSON();
-var baseTimeUnit = JSDATA["baseTimeUnit"].evalJSON();
-
 function addTimeUnitsTableRow() {
   var table = getIxTableById('timeUnitsTable');
-  var rowIndex = table.addRow([ '', false, 0, '', '', '', -1 ]);
-  for ( var i = 0; i < table.getColumnCount(); i++) {
-    table.setCellEditable(rowIndex, i, true);
-  }
-
+  var rowIndex = table.addRow([ '', false, 0, '', '' ]);
+  
   if (rowIndex == 0) {
     table.setCellValue(0, table.getNamedColumnIndex("baseUnit"), true);
     table.setCellValue(0, table.getNamedColumnIndex("baseUnits"), 1);
@@ -17,120 +11,63 @@ function addTimeUnitsTableRow() {
   $('noTimeUnitsAddedMessageContainer').setStyle({
     display : 'none'
   });
-  table.showCell(rowIndex, table.getNamedColumnIndex('removeButton'));
 }
 
 function onLoad(event) {
   var timeUnitsTable = new IxTable($('timeUnitsTable'), {
     id : "timeUnitsTable",
     columns : [
-        {
-          left : 8,
-          width : 30,
-          dataType : 'button',
-          imgsrc : GLOBAL_contextPath + '/gfx/accessories-text-editor.png',
-          tooltip : getLocale().getText("settings.timeUnits.timeUnitsTableEditTooltip"),
-          onclick : function(event) {
-            var table = event.tableComponent;
-            for ( var i = 0; i < table.getColumnCount(); i++) {
-              table.setCellEditable(event.row, i, table.isCellEditable(event.row, i) == false);
-            }
+      {
+        left : 8,
+        width : 30,
+        dataType : 'button',
+        imgsrc : GLOBAL_contextPath + '/gfx/accessories-text-editor.png',
+        tooltip : getLocale().getText("settings.timeUnits.timeUnitsTableEditTooltip"),
+        onclick : function(event) {
+          var table = event.tableComponent;
+          for ( var i = 0; i < table.getColumnCount(); i++) {
+            table.setCellEditable(event.row, i, table.isCellEditable(event.row, i) == false);
+          }
+        }
+      }, {
+        header : getLocale().getText("settings.timeUnits.timeUnitsTableBaseUnitHeader"),
+        left : 38,
+        width : 80,
+        dataType : 'checkbox',
+        editable : true,
+        paramName : 'baseUnit'
+      }, {
+        header : getLocale().getText("settings.timeUnits.timeUnitsTableBaseUnitsHeader"),
+        left : 126,
+        width : 100,
+        dataType : 'number',
+        editable : true,
+        paramName : 'baseUnits',
+        required : true
+      }, {
+        header : getLocale().getText("settings.timeUnits.timeUnitsTableNameHeader"),
+        left : 234,
+        right : 46,
+        dataType : 'text',
+        editable : true,
+        paramName : 'name',
+        required : true
+      }, {
+        right : 8,
+        width : 30,
+        dataType : 'button',
+        imgsrc : GLOBAL_contextPath + '/gfx/list-remove.png',
+        tooltip : getLocale().getText("settings.timeUnits.timeUnitsTableRemoveTooltip"),
+        onclick : function(event) {
+          event.tableComponent.deleteRow(event.row);
+          if (event.tableComponent.getRowCount() == 0) {
+            $('noTimeUnitsAddedMessageContainer').setStyle({
+              display : ''
+            });
           }
         },
-        {
-          header : getLocale().getText("settings.timeUnits.timeUnitsTableBaseUnitHeader"),
-          left : 38,
-          width : 80,
-          dataType : 'checkbox',
-          editable : false,
-          paramName : 'baseUnit'
-        },
-        {
-          header : getLocale().getText("settings.timeUnits.timeUnitsTableBaseUnitsHeader"),
-          left : 126,
-          width : 100,
-          dataType : 'number',
-          editable : false,
-          paramName : 'baseUnits',
-          required : true
-        },
-        {
-          header : getLocale().getText("settings.timeUnits.timeUnitsTableNameHeader"),
-          left : 234,
-          right : 46,
-          dataType : 'text',
-          editable : false,
-          paramName : 'name',
-          required : true
-        },
-        {
-          right : 8,
-          width : 30,
-          dataType : 'button',
-          imgsrc : GLOBAL_contextPath + '/gfx/edit-delete.png',
-          tooltip : getLocale().getText("settings.timeUnits.timeUnitsTableArchiveTooltip"),
-          onclick : function(event) {
-            var table = event.tableComponent;
-            var timeUnitId = table.getCellValue(event.row, table.getNamedColumnIndex('timeUnitId'));
-            var timeUnitName = table.getCellValue(event.row, table.getNamedColumnIndex('name'));
-            var url = GLOBAL_contextPath + "/simpledialog.page?localeId=settings.timeUnits.timeUnitArchiveConfirmDialogContent&localeParams="
-                + encodeURIComponent(timeUnitName);
-
-            var archivedRowIndex;
-            archivedRowIndex = event.row;
-
-            var dialog = new IxDialog({
-              id : 'confirmRemoval',
-              contentURL : url,
-              centered : true,
-              showOk : true,
-              showCancel : true,
-              autoEvaluateSize : true,
-              title : getLocale().getText("settings.timeUnits.timeUnitArchiveConfirmDialogTitle"),
-              okLabel : getLocale().getText("settings.timeUnits.timeUnitArchiveConfirmDialogOkLabel"),
-              cancelLabel : getLocale().getText("settings.timeUnits.timeUnitArchiveConfirmDialogCancelLabel")
-            });
-
-            dialog.addDialogListener(function(event) {
-              switch (event.name) {
-                case 'okClick':
-                  JSONRequest.request("settings/archivetimeunit.json", {
-                    parameters : {
-                      timeUnitId : timeUnitId
-                    },
-                    onSuccess : function(jsonResponse) {
-                      getIxTableById('timeUnitsTable').deleteRow(archivedRowIndex);
-                      saveFormDraft();
-                    }
-                  });
-                break;
-              }
-            });
-
-            dialog.open();
-          },
-          paramName : 'archiveButton',
-          hidden : true
-        }, {
-          right : 8,
-          width : 30,
-          dataType : 'button',
-          imgsrc : GLOBAL_contextPath + '/gfx/list-remove.png',
-          tooltip : getLocale().getText("settings.timeUnits.timeUnitsTableRemoveTooltip"),
-          onclick : function(event) {
-            event.tableComponent.deleteRow(event.row);
-            if (event.tableComponent.getRowCount() == 0) {
-              $('noTimeUnitsAddedMessageContainer').setStyle({
-                display : ''
-              });
-            }
-          },
-          paramName : 'removeButton',
-          hidden : true
-        }, {
-          dataType : 'hidden',
-          paramName : 'timeUnitId'
-        } ]
+        paramName : 'removeButton',
+      } ]
   });
 
   timeUnitsTable.addListener("cellValueChange", function(event) {
@@ -156,18 +93,6 @@ function onLoad(event) {
       }
     }
   });
-
-  var rowIndex;
-  timeUnitsTable.detachFromDom();
-  for ( var i = 0, l = timeUnits.length; i < l; i++) {
-    var timeUnit = timeUnits[i];
-    rowIndex = timeUnitsTable.addRow([ '', timeUnit.id == baseTimeUnit.id, timeUnit.baseUnits, jsonEscapeHTML(timeUnit.name), '', '', timeUnit.id ]);
-    if (timeUnit.id == baseTimeUnit.id) {
-      timeUnitsTable.hideCell(rowIndex, timeUnitsTable.getNamedColumnIndex("baseUnits"));
-    }
-    timeUnitsTable.showCell(rowIndex, timeUnitsTable.getNamedColumnIndex('archiveButton'));
-  }
-  timeUnitsTable.reattachToDom();
 
   if (timeUnitsTable.getRowCount() > 0) {
     $('noTimeUnitsAddedMessageContainer').setStyle({
