465e53d4594fb2471050df936d237c485fa285f7
Antti Leppä <antti.leppa@foyt.fi>
2014-03-13 12:23:07 +0200

    ** Removed useless edit button from setup wizard course participation types view

--- pyramus/src/main/webapp/scripts/gui/system/setupwizard/courseparticipationtypes.js
+++ pyramus/src/main/webapp/scripts/gui/system/setupwizard/courseparticipationtypes.js
@@ -1,10 +1,6 @@
 function addCourseParticipationTypesTableRow() {
   var table = getIxTableById('courseParticipationTypesTable');
-  var rowIndex = table.addRow([ '', false, '', '' ]);
-  for (var i = 0; i < table.getColumnCount(); i++) {
-    table.setCellEditable(rowIndex, i, true);
-  }
-
+  var rowIndex = table.addRow([ false, '', '' ]);
   if (rowIndex == 0) {
     table.setCellValue(0, table.getNamedColumnIndex("initialType"), true);
   }
@@ -15,51 +11,38 @@ function addCourseParticipationTypesTableRow() {
 }
 
 function onLoad(event) {
-  var courseParticipationTypesTable = new IxTable($('courseParticipationTypesTable'), {
+  new IxTable($('courseParticipationTypesTable'), {
     id : "courseParticipationTypesTable",
-    columns : [
-        {
-          left : 8,
-          width : 30,
-          dataType : 'button',
-          imgsrc : GLOBAL_contextPath + '/gfx/accessories-text-editor.png',
-          tooltip : getLocale().getText("system.setupwizard.courseparticipationtypes.courseParticipationTypesTableEditTooltip"),
-          onclick : function(event) {
-            var table = event.tableComponent;
-            for (var i = 0; i < table.getColumnCount(); i++) {
-              table.setCellEditable(event.row, i, table.isCellEditable(event.row, i) == false);
-            }
-          }
-        }, {
-          header : getLocale().getText("system.setupwizard.courseparticipationtypes.courseParticipationTypesTableInitialTypeHeader"),
-          left : 38,
-          width : 80,
-          dataType : 'checkbox',
-          editable : false,
-          paramName : 'initialType'
-        }, {
-          header : getLocale().getText("system.setupwizard.courseparticipationtypes.courseParticipationTypesTableNameHeader"),
-          left : 234,
-          right : 46,
-          dataType : 'text',
-          editable : false,
-          paramName : 'name',
-          required : true
-        }, {
-          right : 8,
-          width : 30,
-          dataType : 'button',
-          imgsrc : GLOBAL_contextPath + '/gfx/list-remove.png',
-          tooltip : getLocale().getText("system.setupwizard.courseparticipationtypes.courseParticipationTypesTableRemoveTooltip"),
-          onclick : function(event) {
-            event.tableComponent.deleteRow(event.row);
-            if (event.tableComponent.getRowCount() == 0) {
-              $('noCourseParticipationTypesAddedMessageContainer').setStyle({
-                display : ''
-              });
-            }
-          },
-          paramName : 'removeButton'
-        } ]
+    columns : [{
+      header : getLocale().getText("system.setupwizard.courseparticipationtypes.courseParticipationTypesTableInitialTypeHeader"),
+      left : 8,
+      width : 100,
+      dataType : 'checkbox',
+      editable : true,
+      paramName : 'initialType'
+    }, {
+      header : getLocale().getText("system.setupwizard.courseparticipationtypes.courseParticipationTypesTableNameHeader"),
+      left : 108,
+      right : 46,
+      dataType : 'text',
+      editable : true,
+      paramName : 'name',
+      required : true
+    }, {
+      right : 8,
+      width : 30,
+      dataType : 'button',
+      imgsrc : GLOBAL_contextPath + '/gfx/list-remove.png',
+      tooltip : getLocale().getText("system.setupwizard.courseparticipationtypes.courseParticipationTypesTableRemoveTooltip"),
+      onclick : function(event) {
+        event.tableComponent.deleteRow(event.row);
+        if (event.tableComponent.getRowCount() == 0) {
+          $('noCourseParticipationTypesAddedMessageContainer').setStyle({
+            display : ''
+          });
+        }
+      },
+      paramName : 'removeButton'
+    } ]
   });
 }
