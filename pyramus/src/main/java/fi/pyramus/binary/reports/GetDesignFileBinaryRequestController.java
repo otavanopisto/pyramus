f0c06dccda6de12ea49f59c02fe4a7e0b5332e20
Antti Leppä <antti.leppa@foyt.fi>
2014-03-24 14:59:56 +0200

    ** Changed reports/getdesignfile binary controller to use magic key based authorization

--- pyramus/src/main/java/fi/pyramus/binary/reports/GetDesignFileBinaryRequestController.java
+++ pyramus/src/main/java/fi/pyramus/binary/reports/GetDesignFileBinaryRequestController.java
@@ -11,7 +11,9 @@ import fi.internetix.smvc.StatusCode;
 import fi.internetix.smvc.controllers.BinaryRequestContext;
 import fi.internetix.smvc.controllers.RequestContext;
 import fi.pyramus.dao.DAOFactory;
+import fi.pyramus.dao.base.MagicKeyDAO;
 import fi.pyramus.dao.reports.ReportDAO;
+import fi.pyramus.domainmodel.base.MagicKey;
 import fi.pyramus.domainmodel.reports.Report;
 import fi.pyramus.framework.BinaryRequestController;
 import fi.pyramus.framework.UserRole;
@@ -37,11 +39,16 @@ public class GetDesignFileBinaryRequestController extends BinaryRequestControlle
   
   @Override
   public void authorize(RequestContext requestContext) throws LoginRequiredException, AccessDeniedException {
-    String reportskey = System.getProperty("pyramus-reports-key");
-    if (StringUtils.isNotBlank(reportskey)) {
-      String authorization = "PyramusReportsKey " + reportskey;
-      if (authorization.equals(requestContext.getRequest().getHeader("Authorization"))) {
-        return;
+    String authorizationHeader = requestContext.getRequest().getHeader("Authorization");
+    if (StringUtils.startsWith(authorizationHeader, "MagicKey ")) {
+      String headerKey = StringUtils.substring(authorizationHeader, 9);
+      if (StringUtils.isNotBlank(headerKey)) {
+        MagicKeyDAO magicKeyDAO = DAOFactory.getInstance().getMagicKeyDAO();
+        MagicKey magicKey = magicKeyDAO.findByName(headerKey);
+        if (magicKey != null) {
+          magicKeyDAO.delete(magicKey);
+          return;
+        }
       }
     }
     
