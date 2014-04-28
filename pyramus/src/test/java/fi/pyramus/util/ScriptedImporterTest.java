6a69770431aa9925cc87c2784cb343648700d446
ilmoeuro <ilmo.euro@gmail.com>
2014-04-28 10:30:50 +0300

    ** Removed test that uses old Faker API

--- pyramus/src/test/java/fi/pyramus/util/ScriptedImporterTest.java
+++ pyramus/src/test/java/fi/pyramus/util/ScriptedImporterTest.java
@@ -12,19 +12,4 @@ import fi.pyramus.util.dataimport.scripting.InvalidScriptException;
 import fi.pyramus.util.dataimport.scripting.ScriptedImporter;
 
 public class ScriptedImporterTest {
-
-  @Test
-  public void testApiLog() throws InvalidScriptException {
-    ScriptedImporter si = new ScriptedImporter();
-    si.importDataFromReader(
-        new StringReader
-        (
-            "({" +
-            "  'run': function(api) {" +
-            "     api.log(api.faker.name());" +
-            "     api.log(api.faker.streetAddress(false));" +
-            "  }" +
-            "})"
-    ));
-  }
 }
