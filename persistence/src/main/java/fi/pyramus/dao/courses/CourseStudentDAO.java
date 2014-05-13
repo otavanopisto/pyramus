d7e62f3b6d81dd56cc9432b2dd8a3b2561280df7
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2014-05-13 15:03:58 +0300

    - Added listByModulesAndStudent to CourseStudentDAO

--- persistence/src/main/java/fi/pyramus/dao/courses/CourseStudentDAO.java
+++ persistence/src/main/java/fi/pyramus/dao/courses/CourseStudentDAO.java
@@ -167,6 +167,28 @@ public class CourseStudentDAO extends PyramusEntityDAO<CourseStudent> {
     return entityManager.createQuery(criteria).getResultList();
   }
   
+  public List<CourseStudent> listByModulesAndStudent(List<Module> modules, Student student) {
+    EntityManager entityManager = getEntityManager(); 
+    
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<CourseStudent> criteria = criteriaBuilder.createQuery(CourseStudent.class);
+    Root<CourseStudent> root = criteria.from(CourseStudent.class);
+    Join<CourseStudent, Student> studentJoin = root.join(CourseStudent_.student);
+    Join<CourseStudent, Course> courseJoin = root.join(CourseStudent_.course);
+    
+    criteria.select(root);
+    criteria.where(
+        criteriaBuilder.and(
+            courseJoin.get(Course_.module).in(modules),
+            criteriaBuilder.equal(root.get(CourseStudent_.student), student),
+            criteriaBuilder.equal(root.get(CourseStudent_.archived), Boolean.FALSE),
+            criteriaBuilder.equal(courseJoin.get(Course_.archived), Boolean.FALSE),
+            criteriaBuilder.equal(studentJoin.get(Student_.archived), Boolean.FALSE)
+        ));
+    
+    return entityManager.createQuery(criteria).getResultList();
+  }
+  
   public List<CourseStudent> listByStudent(Student student) {
     EntityManager entityManager = getEntityManager(); 
     
