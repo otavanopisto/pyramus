0b015173c0fb1f3c9b595f18cd2b15faf471b350
Antti Leppä <antti.leppa@foyt.fi>
2014-06-12 10:30:03 +0300

    ** Added test for create course REST end-point ** Fixed tests to work with changed subject id ** Added temporary hard-coded getLoggedUser method into AbstractRESTService

--- pyramus/src/test/java/fi/pyramus/rest/CoursesRESTServiceTestIT.java
+++ pyramus/src/test/java/fi/pyramus/rest/CoursesRESTServiceTestIT.java
@@ -2,63 +2,82 @@ package fi.pyramus.rest;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 import java.io.IOException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.util.Date;
+
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.jayway.restassured.response.Response;
+
+import fi.pyramus.rest.model.Course;
+import fi.pyramus.rest.model.CourseLength;
+
 public class CoursesRESTServiceTestIT extends AbstractRESTServiceTest {
 
-//CourseRESTServiceTest Course
-//  
-// @Test
-// public void testCreateCourse() throws ClientProtocolException, IOException, ParseException {
-//   SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//   Date beginDate = sdf.parse("2013-02-13");
-//   Date endDate = sdf.parse("2013-05-09");
-//   Date enrolmentTimeEnd = sdf.parse("2013-01-31");
-//   CourseEntity courseEntity = new CourseEntity();
-//   courseEntity.setModule_id(1l);
-//   courseEntity.setName("Test course");
-//   courseEntity.setNameExtension("Test extension");
-//   courseEntity.setState_id(1l);
-//   courseEntity.setSubject_id(1l);
-//   courseEntity.setCourseNumber(2);
-//   courseEntity.setBeginDate(beginDate);
-//   courseEntity.setEndDate(endDate);
-//   courseEntity.setCourseLength_id(1l);
-//   courseEntity.setCourseLength(15d);
-//   courseEntity.setDistanceTeachingDays(10d);
-//   courseEntity.setLocalTeachingDays(5d);
-//   courseEntity.setTeachingHours(60d);
-//   courseEntity.setPlanningHours(15d);
-//   courseEntity.setAssessingHours(5d);
-//   courseEntity.setDescription("Testing how courses work");
-//   courseEntity.setMaxParticipantCount(25l);
-//   courseEntity.setEnrolmentTimeEnd(enrolmentTimeEnd);
-// 
-//   HttpResponse response = doPostRequest("/courses/courses/", courseEntity);
-// 
-//   assertEquals(200, response.getStatusLine().getStatusCode());
-//   HttpEntity entity = response.getEntity();
-//   try {
-//     assertNotNull(entity);
-//     assertEquals("application/json", entity.getContentType().getValue());
-//     courseEntity = unserializeEntity(CourseEntity.class, EntityUtils.toString(entity));
-//     assertNotNull(courseEntity);
-//     assertEquals((Long) 2l, courseEntity.getId());
-//     assertEquals("Test course", courseEntity.getName());
-//     assertEquals(javax.xml.bind.DatatypeConverter.parseDateTime("2013-02-13T00:00:00").getTime(), courseEntity.getBeginDate());
-//     assertEquals("Testing how courses work", courseEntity.getDescription());
-//   } finally {
-//     EntityUtils.consume(entity);
-//   }
-// }
-//  
+  @Test
+  public void testCreateCourse() {
+    // id, length id
+    
+    CourseLength length = new CourseLength(777d, 1l);
+    
+    Course course = new Course(
+        "Create test", 
+        new Date(), 
+        new Date(), 
+        "Course for testing course creation", 
+        Boolean.FALSE, 
+        111, 
+        222l,
+        new Date(),
+        new Date(),
+        "Extension",
+        333d,
+        444d,
+        555d,
+        666d,
+        777d,
+        new Date(),
+        1l, 
+        1l,
+        1l,
+        length, 
+        1l, 
+        1l, 
+        null);
 
+    Response response = given()
+      .contentType("application/json")
+      .body(course)
+      .post("/courses/courses/");
+    
+    response.then()
+      .body("id", not(is((Long) null)))
+      .body("name", is(course.getName()))
+      .body("courseNumber", is( course.getCourseNumber()))
+      .body("description", is( course.getDescription() ))
+      .body("length.units", is( course.getLength().getUnits().floatValue() ))
+      .body("length.unitId", is( course.getLength().getUnitId().intValue() ))
+      .body("creatorId", is( course.getCreatorId().intValue() ))
+      .body("lastModifierId", is( course.getLastModifierId().intValue() ))
+      .body("subjectId", is( course.getSubjectId().intValue() ))
+      .body("maxParticipantCount", is( course.getMaxParticipantCount().intValue() ))
+      .body("archived", is( course.getArchived() ));
+      
+    int id = response.body().jsonPath().getInt("id");
+    
+    given()
+      .delete("/courses/courses/{ID}?permanent=true", id)
+      .then()
+      .statusCode(204);
+  }
+  
   @Test
   public void testGetCourse() {
     given()
@@ -74,7 +93,7 @@ public class CoursesRESTServiceTestIT extends AbstractRESTServiceTest {
       .body("length.unitId", is( 1 ))
       .body("creatorId", is( 1 ))
       .body("lastModifierId", is( 1 ))
-      .body("subject.id", is( 1 ))
+      .body("subjectId", is( 1 ))
       .body("maxParticipantCount", is( 200 ))
       .body("archived", is( false ));
   }
@@ -118,7 +137,7 @@ public class CoursesRESTServiceTestIT extends AbstractRESTServiceTest {
       .body("length[0].unitId", is( 1 ))
       .body("creatorId[0]", is( 1 ))
       .body("lastModifierId[0]", is( 1 ))
-      .body("subject[0].id", is( 1 ))
+      .body("subjectId[0]", is( 1 ))
       .body("maxParticipantCount[0]", is( 100 ))
       .body("archived[0]", is( false ))
       .body("id[1]", is(1001) )
@@ -131,7 +150,7 @@ public class CoursesRESTServiceTestIT extends AbstractRESTServiceTest {
       .body("length[1].unitId", is( 1 ))
       .body("creatorId[1]", is( 1 ))
       .body("lastModifierId[1]", is( 1 ))
-      .body("subject[1].id", is( 1 ))
+      .body("subjectId[1]", is( 1 ))
       .body("maxParticipantCount[1]", is( 200 ))
       .body("archived[1]", is( false ));
   }
--- rest/src/main/java/fi/pyramus/rest/AbstractRESTService.java
+++ rest/src/main/java/fi/pyramus/rest/AbstractRESTService.java
@@ -3,18 +3,20 @@ package fi.pyramus.rest;
 import javax.inject.Inject;
 
 import fi.pyramus.dao.users.UserDAO;
-import fi.pyramus.domainmodel.users.Role;
 import fi.pyramus.domainmodel.users.User;
 
 public abstract class AbstractRESTService {
-
+  
   @Inject
-  UserDAO userDAO;
+  private UserDAO userDAO;
 
-  protected User getUser() {
-    Role role = Role.getRole(4);
-    User user = userDAO.create("Master", "Splinter", "Hamato Yoshi", "Turtles", role);
-    return user;
+  // TODO: Implement this
+  protected Long getLoggedUserId() {
+    return 1l;
+  }
+  
+  protected User getLoggedUser() {
+    return userDAO.findById(getLoggedUserId());
   }
 
 }
