a7fb443778ff9e8209ae985f930ebdf8a9051cfd
Antti Leppä <antti.leppa@foyt.fi>
2014-06-12 13:02:18 +0300

    ** Added more test data for course / module component testing

--- pyramus/src/test/resources/data.sql
+++ pyramus/src/test/resources/data.sql
@@ -28,7 +28,12 @@ values
 insert into
   EducationalLength (id, units, unit, version)
 values 
-  (1, 1, 1, 1);
+  (1, 1, 1, 1),
+  (2, 123, 1, 1),
+  (3, 234, 1, 1),
+  (4, 345, 1, 1),
+  (5, 456, 1, 1),
+  (6, 567, 1, 1);
   
 insert into 
   CourseBase (id, name, archived, courseNumber, created, lastModified, description, courseLength, creator, lastModifier, subject, version, maxParticipantCount)
@@ -41,6 +46,18 @@ values
   (1);
   
 insert into 
+  ComponentBase (id, name, description, length, archived, version) 
+values 
+  (1, 'Test Module #1 component #1', 'Module component for testing', 1, false, 1),
+  (2, 'Test Module #1 component #2', 'Module component for testing', 2, false, 1);
+  
+insert into 
+  ModuleComponent (id, module, indexColumn)
+values 
+  (1, 1, 0),
+  (2, 1, 1);
+
+insert into 
   CourseBase (id, name, archived, courseNumber, created, lastModified, description, courseLength, creator, lastModifier, subject, version, maxParticipantCount)
 values
   (1000, 'Test Course #1', false, 1, PARSEDATETIME('1 1 2010', 'd M yyyy'), PARSEDATETIME('1 1 2010', 'd M yyyy'), 'Course #1 for testing', 1, 1, 1, 1, 1, 100),
@@ -51,6 +68,22 @@ insert into
 values 
   (1000, PARSEDATETIME('2 2 2010', 'd M yyyy'), PARSEDATETIME('3 3 2010', 'd M yyyy'), 10, 'Ext', 1, 1, 40, 30, 20, 10, PARSEDATETIME('1 1 2010', 'd M yyyy')),
   (1001, PARSEDATETIME('2 2 2011', 'd M yyyy'), PARSEDATETIME('3 3 2011', 'd M yyyy'), 20, 'ABC', 1, 2, 15, 17, 20, 10, PARSEDATETIME('1 1 2010', 'd M yyyy'));
+
+insert into 
+  ComponentBase (id, name, description, length, archived, version) 
+values 
+  (1000, 'Test Course #1 component #1', 'Course component for testing', 3, false, 1),
+  (1001, 'Test Course #1 component #2', 'Course component for testing', 4, false, 1),
+  (1002, 'Test Course #2 component #1', 'Course component for testing', 5, false, 1),
+  (1003, 'Test Course #2 component #2', 'Course component for testing', 6, false, 1);
+  
+insert into 
+  CourseComponent (id, course, indexColumn)
+values 
+  (1000, 1000, 0),
+  (1001, 1000, 1),
+  (1002, 1001, 0),
+  (1003, 1001, 1);
   
 insert into hibernate_sequences (sequence_name, sequence_next_hi_value) select 'User', max(id) + 1 from User;
 insert into hibernate_sequences (sequence_name, sequence_next_hi_value) select 'EducationType', max(id) + 1 from EducationType;
@@ -59,6 +92,7 @@ insert into hibernate_sequences (sequence_name, sequence_next_hi_value) select '
 insert into hibernate_sequences (sequence_name, sequence_next_hi_value) select 'EducationalTimeUnit', max(id) + 1 from EducationalTimeUnit;
 insert into hibernate_sequences (sequence_name, sequence_next_hi_value) select 'CourseBase', max(id) + 1 from CourseBase;
 insert into hibernate_sequences (sequence_name, sequence_next_hi_value) select 'EducationalLength', max(id) + 1 from EducationalLength;
+insert into hibernate_sequences (sequence_name, sequence_next_hi_value) select 'ComponentBase', max(id) + 1 from ComponentBase;
 
 
 
