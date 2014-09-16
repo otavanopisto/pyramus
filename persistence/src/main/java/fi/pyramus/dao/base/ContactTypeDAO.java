dd75d19fb6303b1dd5234dde91b57fcaa9c21345
Antti Leppä <antti.leppa@foyt.fi>
2014-09-16 11:44:46 +0300

    ** Added method for listing ContactTypes by name into ContactTypeDAO

--- persistence/src/main/java/fi/pyramus/dao/base/ContactTypeDAO.java
+++ persistence/src/main/java/fi/pyramus/dao/base/ContactTypeDAO.java
@@ -1,10 +1,16 @@
 package fi.pyramus.dao.base;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import fi.pyramus.dao.PyramusEntityDAO;
 import fi.pyramus.domainmodel.base.ContactType;
+import fi.pyramus.domainmodel.base.ContactType_;
 
 @Stateless
 public class ContactTypeDAO extends PyramusEntityDAO<ContactType> {
@@ -15,6 +21,20 @@ public class ContactTypeDAO extends PyramusEntityDAO<ContactType> {
     contactType.setName(name);
     entityManager.persist(contactType);
     return contactType;
+  }
+
+  public List<ContactType> listByName(String name) {
+    EntityManager entityManager = getEntityManager(); 
+    
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<ContactType> criteria = criteriaBuilder.createQuery(ContactType.class);
+    Root<ContactType> root = criteria.from(ContactType.class);
+    criteria.select(root);
+    criteria.where(
+        criteriaBuilder.equal(root.get(ContactType_.name), name)
+    );
+    
+    return entityManager.createQuery(criteria).getResultList();
   }
 
   public ContactType update(ContactType contactType, String name) {
