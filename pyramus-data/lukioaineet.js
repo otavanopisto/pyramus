600a8856cdb4533b7a688e32f4e98f1e25110009
Antti Leppä <antti.leppa@foyt.fi>
2014-09-16 06:13:09 +0300

    ** Updated lukioaineet.js, pkaineet.js and vapaasaineet.js data import scripts

--- pyramus-data/lukioaineet.js
+++ pyramus-data/lukioaineet.js
@@ -1,40 +1,50 @@
 ({
 	'run' : function(api){
-	
+	  api.log('Importing subjects...');
+    
+    if (!api.educationTypes.findIdByCode('lukio')) {
+      api.educationTypes.create('lukio', 'Lukio');
+    }
+    
 		var data = [
-			{ 'name' : 'Äidinkieli ja kirjallisuus ', 'id' : 'A', 'level' : 'lukio'},
-			{ 'name' : 'Venäjä', 'id' : 'VEA', 'level' : 'lukio'},
-			{ 'name' : 'Saksa', 'id' : 'SAA', 'level' : 'lukio'},
-			{ 'name' : 'Ruotsi', 'id' : 'RUA', 'level' : 'lukio'},
-			{ 'name' : 'Englanti ', 'id' : 'ENA', 'level' : 'lukio'},
-			{ 'name' : 'Ruotsi', 'id' : 'RUB', 'level' : 'lukio'},
-			{ 'name' : 'Ranska', 'id' : 'RAB2', 'level' : 'lukio'},
-			{ 'name' : 'Espanja', 'id' : 'EAB3', 'level' : 'lukio'},
-			{ 'name' : 'Italia', 'id' : 'IAB3', 'level' : 'lukio'},
-			{ 'name' : 'Ranska', 'id' : 'RAB3', 'level' : 'lukio'},
-			{ 'name' : 'Saksa', 'id' : 'SAB3', 'level' : 'lukio'},
-			{ 'name' : 'Venäjä', 'id' : 'VEB3', 'level' : 'lukio'},
-			{ 'name' : 'Matematiikka, pitkä', 'id' : 'M', 'level' : 'lukio'},
-			{ 'name' : 'matematiikka, lyhyt', 'id' : 'N', 'level' : 'lukio'},
-			{ 'name' : 'Biologia', 'id' : 'BI', 'level' : 'lukio'},
-			{ 'name' : 'Maantiede ', 'id' : 'GE', 'level' : 'lukio'},
-			{ 'name' : 'Fysiikka', 'id' : 'FY', 'level' : 'lukio'},
-			{ 'name' : 'Kemia', 'id' : 'KE', 'level' : 'lukio'},
-			{ 'name' : 'Uskonto', 'id' : 'UE', 'level' : 'lukio'},
-			{ 'name' : 'elämänkatsomustieto', 'id' : 'ET', 'level' : 'lukio'},
-			{ 'name' : 'Filosofia ', 'id' : 'FI', 'level' : 'lukio'},
-			{ 'name' : 'Psykologia', 'id' : 'PS', 'level' : 'lukio'},
-			{ 'name' : 'Historia', 'id' : 'HI', 'level' : 'lukio'},
-			{ 'name' : 'Yhteiskuntaoppi ', 'id' : 'YH', 'level' : 'lukio'},
-			{ 'name' : 'Terveystieto ', 'id' : 'TE', 'level' : 'lukio'},
-			{ 'name' : 'Liikunta ', 'id' : 'LI', 'level' : 'lukio'},
-			{ 'name' : 'Musiikki', 'id' : 'MU', 'level' : 'lukio'},
-			{ 'name' : 'Kuvataide', 'id' : 'KU', 'level' : 'lukio'},
-			{ 'name' : 'Opinto-ohjaus ', 'id' : 'OP', 'level' : 'lukio'}
+			{ 'name' : 'Äidinkieli ja kirjallisuus ', 'code' : 'AI', 'educationType' : 'lukio'},
+			{ 'name' : 'Venäjä', 'code' : 'VEA', 'educationType' : 'lukio'},
+			{ 'name' : 'Saksa', 'code' : 'SAA', 'educationType' : 'lukio'},
+			{ 'name' : 'Ruotsi', 'code' : 'RUA', 'educationType' : 'lukio'},
+			{ 'name' : 'Englanti ', 'code' : 'ENA', 'educationType' : 'lukio'},
+			{ 'name' : 'Ruotsi', 'code' : 'RUB', 'educationType' : 'lukio'},
+			{ 'name' : 'Ranska', 'code' : 'RAB2', 'educationType' : 'lukio'},
+			{ 'name' : 'Espanja', 'code' : 'EAB3', 'educationType' : 'lukio'},
+			{ 'name' : 'Italia', 'code' : 'IAB3', 'educationType' : 'lukio'},
+			{ 'name' : 'Ranska', 'code' : 'RAB3', 'educationType' : 'lukio'},
+			{ 'name' : 'Saksa', 'code' : 'SAB3', 'educationType' : 'lukio'},
+			{ 'name' : 'Venäjä', 'code' : 'VEB3', 'educationType' : 'lukio'},
+			{ 'name' : 'Matematiikka, pitkä', 'code' : 'M', 'educationType' : 'lukio'},
+			{ 'name' : 'matematiikka, lyhyt', 'code' : 'N', 'educationType' : 'lukio'},
+			{ 'name' : 'Biologia', 'code' : 'BI', 'educationType' : 'lukio'},
+			{ 'name' : 'Maantiede ', 'code' : 'GE', 'educationType' : 'lukio'},
+			{ 'name' : 'Fysiikka', 'code' : 'FY', 'educationType' : 'lukio'},
+			{ 'name' : 'Kemia', 'code' : 'KE', 'educationType' : 'lukio'},
+			{ 'name' : 'Uskonto', 'code' : 'UE', 'educationType' : 'lukio'},
+			{ 'name' : 'elämänkatsomustieto', 'code' : 'ET', 'educationType' : 'lukio'},
+			{ 'name' : 'Filosofia ', 'code' : 'FI', 'educationType' : 'lukio'},
+			{ 'name' : 'Psykologia', 'code' : 'PS', 'educationType' : 'lukio'},
+			{ 'name' : 'Historia', 'code' : 'HI', 'educationType' : 'lukio'},
+			{ 'name' : 'Yhteiskuntaoppi ', 'code' : 'YH', 'educationType' : 'lukio'},
+			{ 'name' : 'Terveystieto ', 'code' : 'TE', 'educationType' : 'lukio'},
+			{ 'name' : 'Liikunta ', 'code' : 'LI', 'educationType' : 'lukio'},
+			{ 'name' : 'Musiikki', 'code' : 'MU', 'educationType' : 'lukio'},
+			{ 'name' : 'Kuvataide', 'code' : 'KU', 'educationType' : 'lukio'},
+			{ 'name' : 'Opinto-ohjaus ', 'code' : 'OP', 'educationType' : 'lukio'}
 		];
 		
-		for(var i = 0; i < data.length;i++){
-			api.subject.createSubject(data[i]["id"], data[i]["name"], data[i]["level"]);
+		for (var i = 0; i < data.length;i++) {
+		  var subject = data[i];
+      if (!api.subjects.findIdByCode(subject.code)) {
+        api.subjects.create(subject.code, subject.name, subject.educationType);
+      } else {
+        api.log("Subject '" + subject.code + "' already exists.");
+      }
 		}	
 	}
 })
