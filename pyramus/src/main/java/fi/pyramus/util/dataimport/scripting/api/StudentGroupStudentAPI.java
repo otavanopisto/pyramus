00ccbe7821d37894033122b51b687ed218e9b148
Antti Leppä <antti.leppa@foyt.fi>
2014-09-17 10:59:34 +0300

    ** Added findIdByStudentAndStudentGroup method into scripted importter studentGroupStudents API

--- pyramus/src/main/java/fi/pyramus/util/dataimport/scripting/api/StudentGroupStudentAPI.java
+++ pyramus/src/main/java/fi/pyramus/util/dataimport/scripting/api/StudentGroupStudentAPI.java
@@ -3,6 +3,7 @@ package fi.pyramus.util.dataimport.scripting.api;
 import fi.pyramus.dao.DAOFactory;
 import fi.pyramus.domainmodel.students.Student;
 import fi.pyramus.domainmodel.students.StudentGroup;
+import fi.pyramus.domainmodel.students.StudentGroupStudent;
 import fi.pyramus.domainmodel.users.User;
 import fi.pyramus.util.dataimport.scripting.InvalidScriptException;
 
@@ -29,6 +30,27 @@ public class StudentGroupStudentAPI {
     }
     
     return (DAOFactory.getInstance().getStudentGroupStudentDAO()).create(studentGroup, student, loggedUser).getId();
+  }
+  
+  public Long findIdByStudentAndStudentGroup(Long studentId, Long studentGroupId) throws InvalidScriptException {
+    User loggedUser = DAOFactory.getInstance().getUserDAO().findById(loggedUserId);
+    if (loggedUser == null) {
+      throw new InvalidScriptException("Logged user could not be found");  
+    }
+
+    Student student =  DAOFactory.getInstance().getStudentDAO().findById(studentId);
+    if (student == null) {
+      throw new InvalidScriptException("Could not find student #" + studentId);  
+    }
+    
+    StudentGroup studentGroup = DAOFactory.getInstance().getStudentGroupDAO().findById(studentGroupId);
+    if (studentGroup == null) {
+      throw new InvalidScriptException("Could not find student group #" + studentGroupId);  
+    }
+    
+    StudentGroupStudent studentGroupStudent = DAOFactory.getInstance().getStudentGroupStudentDAO().findByStudentGroupAndStudent(studentGroup, student);
+    
+    return studentGroupStudent != null ? studentGroupStudent.getId() : null;
   }
 
   private Long loggedUserId;
