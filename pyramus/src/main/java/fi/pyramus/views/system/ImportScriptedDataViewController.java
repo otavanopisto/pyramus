078d2f7ee720fa0623a8da8774ab81fb49359271
Antti Leppä <antti.leppa@foyt.fi>
2014-09-17 11:21:28 +0300

    ** Changed scripted import view to redirect back to view and show a message after successful import

--- pyramus/src/main/java/fi/pyramus/views/system/ImportScriptedDataViewController.java
+++ pyramus/src/main/java/fi/pyramus/views/system/ImportScriptedDataViewController.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 
 import fi.internetix.smvc.SmvcRuntimeException;
 import fi.internetix.smvc.controllers.PageRequestContext;
+import fi.pyramus.I18N.Messages;
 import fi.pyramus.framework.PyramusFormViewController;
 import fi.pyramus.framework.UserRole;
 import fi.pyramus.util.dataimport.scripting.ScriptedImporter;
@@ -12,6 +13,10 @@ public class ImportScriptedDataViewController extends PyramusFormViewController 
 
   @Override
   public void processForm(PageRequestContext requestContext) {
+    if ("SUCCESS".equals(requestContext.getString("status"))) {
+      requestContext.getRequest().setAttribute("statusMessage", Messages.getInstance().getText(requestContext.getRequest().getLocale(), "system.importscripteddata.successMessage"));
+    }
+    
     requestContext.setIncludeJSP("/templates/system/importscripteddata.jsp");
   }
 
@@ -25,7 +30,7 @@ public class ImportScriptedDataViewController extends PyramusFormViewController 
       throw new SmvcRuntimeException(e);
     }
     
-    requestContext.setRedirectURL(requestContext.getRequest().getContextPath() + "/index.page");
+    requestContext.setRedirectURL(requestContext.getRequest().getContextPath() + "/system/importscripteddata.page?status=SUCCESS");
   }
   
   public UserRole[] getAllowedRoles() {
