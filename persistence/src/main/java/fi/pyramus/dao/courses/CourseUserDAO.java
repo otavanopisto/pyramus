06d809321f6e90ff39b2b2da22c12e3ebe540e59
Antti Leppä <antti.leppa@foyt.fi>
2014-09-30 08:35:33 +0300

    ** Removed operations beloning to another entities from CourseUserDAO (create, update, delete)

--- persistence/src/main/java/fi/pyramus/dao/courses/CourseUserDAO.java
+++ persistence/src/main/java/fi/pyramus/dao/courses/CourseUserDAO.java
@@ -11,27 +11,10 @@ import javax.persistence.criteria.Root;
 import fi.pyramus.dao.PyramusEntityDAO;
 import fi.pyramus.domainmodel.courses.Course;
 import fi.pyramus.domainmodel.courses.CourseUser;
-import fi.pyramus.domainmodel.courses.CourseUserRole;
 import fi.pyramus.domainmodel.courses.CourseUser_;
-import fi.pyramus.domainmodel.users.User;
 
 @Stateless
 public class CourseUserDAO extends PyramusEntityDAO<CourseUser> {
-
-  public CourseUser create(Course course, User user, CourseUserRole role) {
-    EntityManager entityManager = getEntityManager();
-
-    CourseUser courseUser = new CourseUser();
-    courseUser.setCourse(course);
-    courseUser.setUser(user);
-    courseUser.setRole(role);
-    entityManager.persist(courseUser);
-
-    course.addCourseUser(courseUser);
-    entityManager.persist(course);
-
-    return courseUser;
-  }
 
   public List<CourseUser> listByCourse(Course course) {
     EntityManager entityManager = getEntityManager(); 
@@ -46,22 +29,5 @@ public class CourseUserDAO extends PyramusEntityDAO<CourseUser> {
     
     return entityManager.createQuery(criteria).getResultList();
   }
-
-  public void update(CourseUser courseUser, User user, CourseUserRole role) {
-    EntityManager entityManager = getEntityManager();
-    courseUser.setUser(user);
-    courseUser.setRole(role);
-    entityManager.persist(courseUser);
-  }
-
-  @Override
-  public void delete(CourseUser courseUser) {
-    EntityManager entityManager = getEntityManager();
-    Course course = courseUser.getCourse();
-    course.removeCourseUser(courseUser);
-    entityManager.persist(course);
-    entityManager.remove(courseUser);
-  }
-
 
 }
