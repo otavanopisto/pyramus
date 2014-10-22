33d65286cceb9d70540ff7390d356e74cf4ea2f2
Antti Leppä <antti.leppa@foyt.fi>
2014-10-22 08:44:04 +0300

    ** Changed staff member, student, course student and course staff member alterations (update, archive / delete) to send webhook notifications to configured clients

--- pyramus/src/main/java/fi/pyramus/webhooks/WebhookListeners.java
+++ pyramus/src/main/java/fi/pyramus/webhooks/WebhookListeners.java
@@ -6,7 +6,14 @@ import javax.inject.Inject;
 
 import fi.pyramus.events.CourseArchivedEvent;
 import fi.pyramus.events.CourseCreatedEvent;
-import fi.pyramus.events.CourseUpdatedEvent;
+import fi.pyramus.events.CourseStaffMemberCreatedEvent;
+import fi.pyramus.events.CourseStaffMemberDeletedEvent;
+import fi.pyramus.events.CourseStudentArchivedEvent;
+import fi.pyramus.events.CourseStudentCreatedEvent;
+import fi.pyramus.events.StaffMemberCreatedEvent;
+import fi.pyramus.events.StaffMemberDeletedEvent;
+import fi.pyramus.events.StudentArchivedEvent;
+import fi.pyramus.events.StudentCreatedEvent;
 
 @ApplicationScoped
 public class WebhookListeners {
@@ -18,12 +25,40 @@ public class WebhookListeners {
     webhookController.sendWebHookNotifications(new WebhookCourseCreatePayload(event.getCourseId()));
   }
 
-  public void onCourseUpdated(@Observes CourseUpdatedEvent event) {
-    webhookController.sendWebHookNotifications(new WebhookCourseUpdatePayload(event.getCourseId()));
-  }
-
   public void onCourseArchived(@Observes CourseArchivedEvent event) {
     webhookController.sendWebHookNotifications(new WebhookCourseArchivePayload(event.getCourseId()));
+  }
+  
+  public void onStudentCreated(@Observes StudentCreatedEvent event) {
+    webhookController.sendWebHookNotifications(new WebhookStudentCreatePayload(event.getStudentId()));
+  }
+
+  public void onStudentArchived(@Observes StudentArchivedEvent event) {
+    webhookController.sendWebHookNotifications(new WebhookStudentArchivePayload(event.getStudentId()));
+  }
+  
+  public void onCourseStaffMemberCreated(@Observes CourseStaffMemberCreatedEvent event) {
+    webhookController.sendWebHookNotifications(new WebhookCourseStaffMemberCreatePayload(event.getCourseStaffMemberId(), event.getCourseId(), event.getStaffMemberId()));
+  }
+  
+  public void onCourseStaffMemberDeleted(@Observes CourseStaffMemberDeletedEvent event) {
+    webhookController.sendWebHookNotifications(new WebhookCourseStaffMemberDeletePayload(event.getCourseStaffMemberId(), event.getCourseId(), event.getStaffMemberId()));
+  }
+  
+  public void onCourseStudentCreated(@Observes CourseStudentCreatedEvent event) {
+    webhookController.sendWebHookNotifications(new WebhookCourseStudentCreatePayload(event.getCourseStudentId(), event.getCourseId(), event.getStudentId()));
+  }
+  
+  public void onCourseStudentArchived(@Observes CourseStudentArchivedEvent event) {
+    webhookController.sendWebHookNotifications(new WebhookCourseStudentArchivePayload(event.getCourseStudentId(), event.getCourseId(), event.getStudentId()));
+  }
+  
+  public void onStaffMemberCreated(@Observes StaffMemberCreatedEvent event) {
+    webhookController.sendWebHookNotifications(new WebhookStaffMemberCreatePayload(event.getStaffMemberId()));
+  }
+  
+  public void onStaffMemberDeleted(@Observes StaffMemberDeletedEvent event) {
+    webhookController.sendWebHookNotifications(new WebhookStaffMemberDeletePayload(event.getStaffMemberId()));
   }
   
 }
--- webhooks/src/main/java/fi/pyramus/webhooks/WebhookCourseArchivePayload.java
+++ webhooks/src/main/java/fi/pyramus/webhooks/WebhookCourseArchivePayload.java
@@ -5,7 +5,7 @@ import fi.pyramus.webhooks.data.WebhookCourseData;
 public class WebhookCourseArchivePayload extends WebhookPayload<WebhookCourseData> {
 
   public WebhookCourseArchivePayload(Long courseId) {
-    super(WebhookType.COURSE_UPDATE, new WebhookCourseData(courseId));
+    super(WebhookType.COURSE_ARCHIVE, new WebhookCourseData(courseId));
   }
   
 }
