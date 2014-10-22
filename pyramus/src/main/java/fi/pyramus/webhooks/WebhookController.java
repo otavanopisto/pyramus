3128f6903451723eb777f7267b897a2e23182db0
Antti Leppä <antti.leppa@foyt.fi>
2014-10-22 13:01:13 +0300

    ** Changed webhooks to be notified only after successful transaction

--- pyramus/src/main/java/fi/pyramus/webhooks/WebhookController.java
+++ pyramus/src/main/java/fi/pyramus/webhooks/WebhookController.java
@@ -9,7 +9,6 @@ import javax.ejb.Stateless;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -19,37 +18,30 @@ import org.apache.http.util.EntityUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import fi.pyramus.dao.webhooks.WebhookDAO;
-import fi.pyramus.domainmodel.webhooks.Webhook;
+import fi.pyramus.webhooks.Webhook;
 
 @Stateless
 @Dependent
 public class WebhookController {
-  
+
   @Inject
   private Logger logger;
-  
-  @Inject
-  private WebhookDAO webhookDAO;
 
-  public void sendWebhookNotifications(WebhookPayload<?> payload) {
-    List<Webhook> webhooks = webhookDAO.listAll();
+  public void sendWebhookNotifications(List<Webhook> webhooks, WebhookPayload<?> payload) {
     if (!webhooks.isEmpty()) {
-      ObjectMapper objectMapper = new ObjectMapper(); 
+      ObjectMapper objectMapper = new ObjectMapper();
       try {
         String data = objectMapper.writeValueAsString(payload);
-        
+
         for (Webhook webhook : webhooks) {
           try {
-            String signature = DigestUtils.md5Hex(webhook.getSecret());
-            
             HttpClient client = new DefaultHttpClient();
-            
+
             HttpPost httpPost = new HttpPost(webhook.getUrl());
             try {
               StringEntity dataEntity = new StringEntity(data);
               try {
-                httpPost.addHeader("X-Pyramus-Signature", signature);
+                httpPost.addHeader("X-Pyramus-Signature", webhook.getSignature());
                 httpPost.setEntity(dataEntity);
                 client.execute(httpPost);
               } finally {
@@ -59,7 +51,7 @@ public class WebhookController {
               httpPost.releaseConnection();
             }
           } catch (IOException e) {
-            logger.log(Level.SEVERE, "Failed to send webhook notification to " + webhook.getSecret(), e);
+            logger.log(Level.SEVERE, "Failed to send webhook notification to " + webhook.getUrl(), e);
           }
         }
       } catch (JsonProcessingException e) {
@@ -67,5 +59,5 @@ public class WebhookController {
       }
     }
   }
-  
+
 }
