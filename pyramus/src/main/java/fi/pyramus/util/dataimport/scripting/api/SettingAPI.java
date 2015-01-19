4be72324b212ec08f3810996a4184bd33df6643a
Antti Leppä <antti.leppa@foyt.fi>
2015-01-19 08:51:44 +0200

    ** Added set and get methods into import settings API

--- pyramus/src/main/java/fi/pyramus/util/dataimport/scripting/api/SettingAPI.java
+++ pyramus/src/main/java/fi/pyramus/util/dataimport/scripting/api/SettingAPI.java
@@ -2,6 +2,7 @@ package fi.pyramus.util.dataimport.scripting.api;
 
 import fi.pyramus.dao.DAOFactory;
 import fi.pyramus.dao.system.SettingDAO;
+import fi.pyramus.dao.system.SettingKeyDAO;
 import fi.pyramus.domainmodel.system.Setting;
 import fi.pyramus.domainmodel.system.SettingKey;
 
@@ -16,6 +17,40 @@ public class SettingAPI {
     SettingKey settingKey = DAOFactory.getInstance().getSettingKeyDAO().findById(settingKeyId);
     Setting setting = settingDAO.create(settingKey, value);
     return setting.getId();
+  }
+  
+  public String get(String key) {
+    SettingDAO settingDAO = DAOFactory.getInstance().getSettingDAO();
+    SettingKeyDAO settingKeyDAO = DAOFactory.getInstance().getSettingKeyDAO();
+    
+    SettingKey settingKey = settingKeyDAO.findByName(key);
+    if (settingKey == null) {
+      return null;
+    }
+    
+    Setting setting = settingDAO.findByKey(settingKey);
+    if (setting != null) {
+      return setting.getValue();
+    }
+    
+    return null;
+  }
+  
+  public void set(String key, String value) {
+    SettingDAO settingDAO = DAOFactory.getInstance().getSettingDAO();
+    SettingKeyDAO settingKeyDAO = DAOFactory.getInstance().getSettingKeyDAO();
+    
+    SettingKey settingKey = settingKeyDAO.findByName(key);
+    if (settingKey == null) {
+      settingKey = settingKeyDAO.create(key);
+    }
+    
+    Setting setting = settingDAO.findByKey(settingKey);
+    if (setting == null) {
+      settingDAO.create(settingKey, value);
+    } else {
+      settingDAO.update(setting, settingKey, value);
+    }
   }
   
   @SuppressWarnings("unused")
