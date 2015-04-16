f3cf45c6201818e832a805760d4443e07d9e99bd
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2015-04-16 09:30:25 +0300

    - Added FIND_CONTACTTYPE right for TRUSTED_SYSTEM

--- rest/src/main/java/fi/pyramus/rest/controller/permissions/CommonPermissions.java
+++ rest/src/main/java/fi/pyramus/rest/controller/permissions/CommonPermissions.java
@@ -183,7 +183,7 @@ public class CommonPermissions extends AbstractPyramusPermissionCollection imple
   public static final String LIST_CONTACTTYPES = "LIST_CONTACTTYPES";
   
   @Scope (PermissionScope.ENVIRONMENT)
-  @DefaultPermissionRoles ({ ADMINISTRATOR, MANAGER, USER, GUEST, STUDENT })
+  @DefaultPermissionRoles ({ ADMINISTRATOR, MANAGER, USER, GUEST, STUDENT, TRUSTED_SYSTEM })
   public static final String FIND_CONTACTTYPE = "FIND_CONTACTTYPE";
   
   @Scope (PermissionScope.ENVIRONMENT)
