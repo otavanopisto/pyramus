44038604bf9c56d19785b2d7e299896cb513a618
pkukkon <pasi.kukkonen@otavanopisto.fi>
2016-10-11 07:51:01 +0300

    - removed identifier from AssessmentRequest (not a real entity)

--- rest-model/src/main/java/fi/otavanopisto/pyramus/rest/model/AssessmentRequest.java
+++ rest-model/src/main/java/fi/otavanopisto/pyramus/rest/model/AssessmentRequest.java
@@ -4,14 +4,6 @@ import java.util.Date;
 
 public class AssessmentRequest {
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
   public String getFirstName() {
     return firstName;
   }
@@ -84,7 +76,6 @@ public class AssessmentRequest {
     this.courseNameExtension = courseNameExtension;
   }
 
-  private Long id;
   private Long userId;
   private String firstName;
   private String lastName;
