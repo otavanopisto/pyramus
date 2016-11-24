4cae3e14ca3668bd7bc82c9c7ad182ca1d02a656
ilmo.euro <ilmo.euro@gmail.com>
2016-11-24 10:11:03 +0200

    ** Fixes #429

--- rest/src/main/java/fi/otavanopisto/pyramus/rest/StudentRESTService.java
+++ rest/src/main/java/fi/otavanopisto/pyramus/rest/StudentRESTService.java
@@ -2562,7 +2562,7 @@ public class StudentRESTService extends AbstractRESTService {
           student)
         && !restSecurity.hasPermission(
             new String[] { StudentPermissions.STUDENT_OWNER },
-            student.getPerson() )) {
+            student )) {
       return Response.status(Status.FORBIDDEN).build();
     }
 
