e83ee43b26da341f744e4172586d69bc3b580ff2
pkukkon <pasi.kukkonen@otavanopisto.fi>
2017-09-13 12:48:31 +0300

    - entity

---
+++ persistence/src/main/java/fi/otavanopisto/pyramus/domainmodel/application/ApplicationLog.java
@@ -0,0 +1,112 @@
+package fi.otavanopisto.pyramus.domainmodel.application;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.TableGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.FullTextFilterDef;
+import org.hibernate.search.annotations.FullTextFilterDefs;
+import org.hibernate.search.annotations.Indexed;
+
+import fi.otavanopisto.pyramus.domainmodel.base.ArchivableEntity;
+import fi.otavanopisto.pyramus.domainmodel.users.User;
+import fi.otavanopisto.pyramus.persistence.search.filters.ArchivedEntityFilterFactory;
+
+@Entity
+@Indexed
+@FullTextFilterDefs (
+  @FullTextFilterDef (
+    name="ArchivedApplicationLog",
+    impl=ArchivedEntityFilterFactory.class
+  )
+)
+public class ApplicationLog implements ArchivableEntity {
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Application getApplication() {
+    return application;
+  }
+
+  public void setApplication(Application application) {
+    this.application = application;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public Boolean getArchived() {
+    return archived;
+  }
+
+  public void setArchived(Boolean archived) {
+    this.archived = archived;
+  }
+
+  @Id
+  private Long id;
+
+  @ManyToOne  
+  @JoinColumn(name="application")
+  private Application application;
+
+  @Lob
+  @NotNull
+  @Column (nullable=false)
+  @Field
+  private String text;
+
+  @ManyToOne  
+  @JoinColumn(name="user")
+  private User user;
+
+  @NotNull
+  @Column (nullable=false)
+  @Temporal (value=TemporalType.TIMESTAMP)
+  private Date date;
+
+  @NotNull
+  @Column (nullable = false)
+  @Field
+  private Boolean archived;
+
+}
