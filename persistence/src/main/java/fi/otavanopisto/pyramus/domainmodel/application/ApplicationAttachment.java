ed2daf0d86bdec62160df95aeeec5fcddf8b0c0f
pkukkon <pasi.kukkonen@otavanopisto.fi>
2017-09-15 14:46:06 +0300

    - entity

---
+++ persistence/src/main/java/fi/otavanopisto/pyramus/domainmodel/application/ApplicationAttachment.java
@@ -0,0 +1,68 @@
+package fi.otavanopisto.pyramus.domainmodel.application;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.TableGenerator;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class ApplicationAttachment {
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getApplicationId() {
+    return applicationId;
+  }
+
+  public void setApplicationId(String applicationId) {
+    this.applicationId = applicationId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getSize() {
+    return size;
+  }
+
+  public void setSize(Integer size) {
+    this.size = size;
+  }
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.TABLE, generator="ApplicationAttachment")
+  @TableGenerator(name="ApplicationAttachment", allocationSize=1, table = "hibernate_sequences", pkColumnName = "sequence_name", valueColumnName = "sequence_next_hi_value")
+  private Long id;
+  
+  @NotNull
+  @Column (nullable = false)
+  @NotEmpty
+  private String applicationId;
+
+  @NotNull
+  @Column (nullable = false)
+  @NotEmpty
+  private String name;
+
+  @NotNull
+  @Column (nullable = false)
+  @NotEmpty
+  private Integer size;
+
+}
