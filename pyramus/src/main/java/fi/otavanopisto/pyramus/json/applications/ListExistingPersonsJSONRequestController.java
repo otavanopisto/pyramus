294c8325d9fc4d4140aa523fa93151ec5c5a9e2d
pkukkon <pasi.kukkonen@otavanopisto.fi>
2017-09-20 14:05:33 +0300

    - imports

--- pyramus/src/main/java/fi/otavanopisto/pyramus/json/applications/ListExistingPersonsJSONRequestController.java
+++ pyramus/src/main/java/fi/otavanopisto/pyramus/json/applications/ListExistingPersonsJSONRequestController.java
@@ -1,11 +1,6 @@
 package fi.otavanopisto.pyramus.json.applications;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
