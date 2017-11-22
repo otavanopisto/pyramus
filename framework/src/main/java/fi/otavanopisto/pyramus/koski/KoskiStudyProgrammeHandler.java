ed12cb2b451777a803f2677d857eda5e9f5c90c8
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2017-11-22 09:35:39 +0200

    aine handlers

--- framework/src/main/java/fi/otavanopisto/pyramus/koski/KoskiStudyProgrammeHandler.java
+++ framework/src/main/java/fi/otavanopisto/pyramus/koski/KoskiStudyProgrammeHandler.java
@@ -4,6 +4,8 @@ public enum KoskiStudyProgrammeHandler {
 
   aikuistenperusopetuksenalkuvaihe,
   lukio,
-  aikuistenperusopetus
+  aikuistenperusopetus,
+  aineopiskelulukio,
+  aineopiskeluperusopetus
   
 }
