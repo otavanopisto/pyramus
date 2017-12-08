91ad6375d0be900f620f5374e0bb715b1cb53d1b
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2017-12-08 09:58:03 +0200

    temp

--- pyramus/src/test/java/fi/otavanopisto/pyramus/koski/aikuistenperusopetus/APOOppiaineenOppimaaraData.java
+++ pyramus/src/test/java/fi/otavanopisto/pyramus/koski/aikuistenperusopetus/APOOppiaineenOppimaaraData.java
@@ -2,7 +2,10 @@ package fi.otavanopisto.pyramus.koski.aikuistenperusopetus;
 
 import java.util.Date;
 
+import fi.otavanopisto.pyramus.koski.AbstractKoskiData;
+import fi.otavanopisto.pyramus.koski.KoodistoViite;
 import fi.otavanopisto.pyramus.koski.koodisto.Kieli;
+import fi.otavanopisto.pyramus.koski.koodisto.OpintojenRahoitus;
 import fi.otavanopisto.pyramus.koski.koodisto.OpiskeluoikeudenTila;
 import fi.otavanopisto.pyramus.koski.koodisto.OppiaineAidinkieliJaKirjallisuus;
 import fi.otavanopisto.pyramus.koski.koodisto.PerusopetuksenSuoritusTapa;
@@ -18,7 +21,7 @@ import fi.otavanopisto.pyramus.koski.model.aikuistenperusopetus.AikuistenPerusop
 import fi.otavanopisto.pyramus.koski.model.aikuistenperusopetus.AikuistenPerusopetuksenOppiaineenTunniste;
 import fi.otavanopisto.pyramus.koski.model.aikuistenperusopetus.PerusopetuksenOppiaineenOppimaaranSuoritus;
 
-public class APOOppiaineenOppimaaraData {
+public class APOOppiaineenOppimaaraData extends AbstractKoskiData {
 
   public static Oppija getTestStudentMinimal() {
     Oppija oppija = new Oppija();
@@ -28,18 +31,45 @@ public class APOOppiaineenOppimaaraData {
     oppija.addOpiskeluoikeus(opiskeluoikeus);
     
     OpiskeluoikeusJakso jakso = new OpiskeluoikeusJakso(new Date(), OpiskeluoikeudenTila.lasna);
-    opiskeluoikeus.getTila().addOpiskeluoikeusJakso(jakso );
+    opiskeluoikeus.getTila().addOpiskeluoikeusJakso(jakso);
     
-    OrganisaationToimipiste toimipiste = new OrganisaationToimipisteOID("1.2.246.562.10.17749422402");
+    OrganisaationToimipiste toimipiste = new OrganisaationToimipisteOID(ACADEMYOID);
     AikuistenPerusopetuksenOppiaineenTunniste koulutusmoduuli = new AikuistenPerusopetuksenOppiaineenSuoritusAidinkieli(
         OppiaineAidinkieliJaKirjallisuus.AI1, false);
     AikuistenPerusopetuksenOppiaineenSuoritus oppiaine = new AikuistenPerusopetuksenOppiaineenSuoritus(koulutusmoduuli);
     PerusopetuksenOppiaineenOppimaaranSuoritus suoritus = new PerusopetuksenOppiaineenOppimaaranSuoritus(
-        PerusopetuksenSuoritusTapa.koulutus, Kieli.FI, toimipiste, SuorituksenTila.KESKEN, oppiaine );
+        PerusopetuksenSuoritusTapa.koulutus, Kieli.FI, toimipiste, SuorituksenTila.KESKEN, oppiaine);
     opiskeluoikeus.addSuoritus(suoritus);
     
     return oppija;
   }
   
+  public static Oppija getTestStudent() {
+    Oppija oppija = new Oppija();
+    oppija.setHenkilo(new HenkiloUusi("111111A111C", "Sally", "Student", "Sally"));
+    
+    AikuistenPerusopetuksenOpiskeluoikeus opiskeluoikeus = new AikuistenPerusopetuksenOpiskeluoikeus();
+    oppija.addOpiskeluoikeus(opiskeluoikeus);
+
+    Date paattymispaiva = date(2017, 10, 10);
+    opiskeluoikeus.setPaattymispaiva(paattymispaiva);
+    
+    OpiskeluoikeusJakso jakso = new OpiskeluoikeusJakso(date(2017, 1, 1), OpiskeluoikeudenTila.lasna);
+    jakso.setOpintojenRahoitus(new KoodistoViite<OpintojenRahoitus>(OpintojenRahoitus.K1));
+    opiskeluoikeus.getTila().addOpiskeluoikeusJakso(jakso);
+    opiskeluoikeus.getTila().addOpiskeluoikeusJakso(new OpiskeluoikeusJakso(paattymispaiva, OpiskeluoikeudenTila.eronnut));
+    
+    OrganisaationToimipiste toimipiste = new OrganisaationToimipisteOID(ACADEMYOID);
+    AikuistenPerusopetuksenOppiaineenTunniste koulutusmoduuli = new AikuistenPerusopetuksenOppiaineenSuoritusAidinkieli(
+        OppiaineAidinkieliJaKirjallisuus.AI1, false);
+    AikuistenPerusopetuksenOppiaineenSuoritus oppiaine = new AikuistenPerusopetuksenOppiaineenSuoritus(koulutusmoduuli);
+    PerusopetuksenOppiaineenOppimaaranSuoritus suoritus = new PerusopetuksenOppiaineenOppimaaranSuoritus(
+        PerusopetuksenSuoritusTapa.koulutus, Kieli.FI, toimipiste, SuorituksenTila.KESKEN, oppiaine);
+    opiskeluoikeus.addSuoritus(suoritus);
+    
+    suoritus.setVahvistus(getVahvistus(paattymispaiva, ACADEMYOID));
+    
+    return oppija;
+  }
   
 }
