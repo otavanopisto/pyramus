656c6721662ac1981ac899572539596f4be5487d
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2017-12-18 12:07:33 +0200

    Fixed?

--- pyramus/src/test/java/fi/otavanopisto/pyramus/koski/KoskiJSONValidatorTestsIT.java
+++ pyramus/src/test/java/fi/otavanopisto/pyramus/koski/KoskiJSONValidatorTestsIT.java
@@ -13,7 +13,7 @@ public class KoskiJSONValidatorTestsIT extends AbstractKoskiTest {
   @Test
   public void testSchemaMatching() throws IOException {
     URL testFile = this.getClass().getResource("test-student.json");
-    String testJSON = IOUtils.toString(testFile);
+    String testJSON = IOUtils.toString(testFile, "UTF-8");
     
     assertThat(testJSON, getSchemaValidator());
   }
