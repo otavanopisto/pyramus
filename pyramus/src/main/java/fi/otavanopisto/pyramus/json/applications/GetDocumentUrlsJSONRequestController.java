85f50519e273baaae38422de3e908880d4d5af6f
pkukkon <pasi.kukkonen@otavanopisto.fi>
2018-01-23 14:18:04 +0200

    - minor issues

--- pyramus/src/main/java/fi/otavanopisto/pyramus/json/applications/GetDocumentUrlsJSONRequestController.java
+++ pyramus/src/main/java/fi/otavanopisto/pyramus/json/applications/GetDocumentUrlsJSONRequestController.java
@@ -1,7 +1,5 @@
 package fi.otavanopisto.pyramus.json.applications;
 
-import java.util.logging.Logger;
-
 import org.apache.commons.lang3.StringUtils;
 
 import fi.internetix.smvc.controllers.JSONRequestContext;
