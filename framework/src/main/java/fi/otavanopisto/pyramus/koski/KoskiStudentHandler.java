d5ed98dbece05eab7a4e96bbe544fdd773699a29
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2018-02-08 08:37:33 +0200

    Fixed issue when grade is null

--- framework/src/main/java/fi/otavanopisto/pyramus/koski/KoskiStudentHandler.java
+++ framework/src/main/java/fi/otavanopisto/pyramus/koski/KoskiStudentHandler.java
@@ -116,25 +116,27 @@ public class KoskiStudentHandler {
   }
 
   protected ArviointiasteikkoYleissivistava getArvosana(Grade grade) {
-    switch (grade.getName()) {
-      case "4":
-        return ArviointiasteikkoYleissivistava.GRADE_4;
-      case "5":
-        return ArviointiasteikkoYleissivistava.GRADE_5;
-      case "6":
-        return ArviointiasteikkoYleissivistava.GRADE_6;
-      case "7":
-        return ArviointiasteikkoYleissivistava.GRADE_7;
-      case "8":
-        return ArviointiasteikkoYleissivistava.GRADE_8;
-      case "9":
-        return ArviointiasteikkoYleissivistava.GRADE_9;
-      case "10":
-        return ArviointiasteikkoYleissivistava.GRADE_10;
-      case "H":
-        return ArviointiasteikkoYleissivistava.GRADE_H;
-      case "S":
-        return ArviointiasteikkoYleissivistava.GRADE_S;
+    if (grade != null) {
+      switch (grade.getName()) {
+        case "4":
+          return ArviointiasteikkoYleissivistava.GRADE_4;
+        case "5":
+          return ArviointiasteikkoYleissivistava.GRADE_5;
+        case "6":
+          return ArviointiasteikkoYleissivistava.GRADE_6;
+        case "7":
+          return ArviointiasteikkoYleissivistava.GRADE_7;
+        case "8":
+          return ArviointiasteikkoYleissivistava.GRADE_8;
+        case "9":
+          return ArviointiasteikkoYleissivistava.GRADE_9;
+        case "10":
+          return ArviointiasteikkoYleissivistava.GRADE_10;
+        case "H":
+          return ArviointiasteikkoYleissivistava.GRADE_H;
+        case "S":
+          return ArviointiasteikkoYleissivistava.GRADE_S;
+      }
     }
     
     return null;
