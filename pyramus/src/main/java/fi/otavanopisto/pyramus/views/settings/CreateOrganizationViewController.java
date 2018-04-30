51cd00765800879df9bb9ef95149d9d522da0c81
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2018-04-30 14:59:48 +0300

    Added organization view controllers

---
+++ pyramus/src/main/java/fi/otavanopisto/pyramus/views/settings/CreateOrganizationViewController.java
@@ -0,0 +1,25 @@
+package fi.otavanopisto.pyramus.views.settings;
+
+import java.util.Locale;
+
+import fi.internetix.smvc.controllers.PageRequestContext;
+import fi.otavanopisto.pyramus.I18N.Messages;
+import fi.otavanopisto.pyramus.breadcrumbs.Breadcrumbable;
+import fi.otavanopisto.pyramus.framework.PyramusViewController;
+import fi.otavanopisto.pyramus.framework.UserRole;
+
+public class CreateOrganizationViewController extends PyramusViewController implements Breadcrumbable {
+
+  public void process(PageRequestContext pageRequestContext) {
+    pageRequestContext.setIncludeJSP("/templates/settings/createorganization.jsp");
+  }
+
+  public UserRole[] getAllowedRoles() {
+    return new UserRole[] { UserRole.MANAGER, UserRole.ADMINISTRATOR };
+  }
+
+  public String getName(Locale locale) {
+    return Messages.getInstance().getText(locale, "settings.createOrganization.pageTitle");
+  }
+
+}
