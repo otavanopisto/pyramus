169c808b75c261a6319529a94364d9f3bc5ed702
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2018-12-21 11:26:34 +0200

    Interface changes

--- rest/src/main/java/fi/otavanopisto/pyramus/rest/controller/permissions/MatriculationPermissions.java
+++ rest/src/main/java/fi/otavanopisto/pyramus/rest/controller/permissions/MatriculationPermissions.java
@@ -3,6 +3,7 @@ package fi.otavanopisto.pyramus.rest.controller.permissions;
 import java.util.List;
 
 import fi.otavanopisto.pyramus.security.impl.AbstractPyramusPermissionCollection;
+import fi.otavanopisto.pyramus.security.impl.CourseRoleArchetype;
 import fi.otavanopisto.pyramus.security.impl.DefaultPermissionRoles;
 import fi.otavanopisto.pyramus.security.impl.PermissionScope;
 import fi.otavanopisto.pyramus.security.impl.PyramusPermissionCollection;
@@ -40,4 +41,10 @@ public class MatriculationPermissions extends AbstractPyramusPermissionCollectio
   public PermissionFeature[] listPermissionFeatures(String permission) throws NoSuchFieldException, SecurityException {
     return super.listPermissionFeatures(MatriculationPermissions.class, permission);
   }
+
+  @Override
+  public CourseRoleArchetype[] getDefaultCourseRoles(String permission) throws NoSuchFieldException {
+    return super.getDefaultCourseRoles(MatriculationPermissions.class, permission);
+  }
+
 }
