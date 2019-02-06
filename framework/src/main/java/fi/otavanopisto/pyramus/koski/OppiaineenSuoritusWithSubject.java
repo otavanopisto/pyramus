fc8b676e5f3e2da2621a4067889c0bd6d7e836e9
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2019-02-06 14:22:31 +0200

    Added Student update to Koski when subject grade changes

--- framework/src/main/java/fi/otavanopisto/pyramus/koski/OppiaineenSuoritusWithSubject.java
+++ framework/src/main/java/fi/otavanopisto/pyramus/koski/OppiaineenSuoritusWithSubject.java
@@ -2,11 +2,10 @@ package fi.otavanopisto.pyramus.koski;
 
 import fi.otavanopisto.pyramus.domainmodel.base.Subject;
 
-public class OppiaineenSuoritusWithCurriculum<T> {
+public class OppiaineenSuoritusWithSubject<T> {
 
-  public OppiaineenSuoritusWithCurriculum(Subject subject, OpiskelijanOPS ops, T oppiaineenSuoritus) {
+  public OppiaineenSuoritusWithSubject(Subject subject, T oppiaineenSuoritus) {
     this.subject = subject;
-    this.ops = ops;
     this.oppiaineenSuoritus = oppiaineenSuoritus;
   }
   
@@ -14,15 +13,10 @@ public class OppiaineenSuoritusWithCurriculum<T> {
     return oppiaineenSuoritus;
   }
 
-  public OpiskelijanOPS getOps() {
-    return ops;
-  }
-
   public Subject getSubject() {
     return subject;
   }
 
   private final Subject subject;
-  private final OpiskelijanOPS ops;
   private final T oppiaineenSuoritus;
 }
