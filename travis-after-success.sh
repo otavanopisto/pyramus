6271b0bba1394720959ff444eca9346333cb6ea2
petja-laitila <petja.laitila@otavanopisto.fi>
2019-10-15 10:42:35 +0300

    sda

--- travis-after-success.sh
+++ travis-after-success.sh
@@ -1,5 +1,5 @@
-if [ $TRAVIS_PULL_REQUEST == false ]; then
+if [[ $TRAVIS_PULL_REQUEST == false ]]; then
   echo Preparing to deploy
-  python travis-prepare-maven.py;
-  mvn deploy --settings ~/.m2/mySettings.xml;
+  python travis-prepare-maven.py
+  mvn deploy --settings ~/.m2/mySettings.xml
 fi;
