2b1c88c82ca6544b1ba7eb2152da1007e45a372b
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2019-11-18 14:19:40 +0200

    Handling for prolonged study end date

--- framework/src/main/java/fi/otavanopisto/pyramus/koski/KoskiStudentHandler.java
+++ framework/src/main/java/fi/otavanopisto/pyramus/koski/KoskiStudentHandler.java
@@ -197,6 +197,16 @@ public abstract class KoskiStudentHandler {
             if (period.getEnd() != null) {
               tila.addOpiskeluoikeusJakso(new OpiskeluoikeusJakso(period.getEnd(), OpiskeluoikeudenTila.lasna));
             }
+          break; 
+          case PROLONGED_STUDYENDDATE:
+            // Pidennetty päättymispäivä on aina muuta kautta rahoitettu
+            OpiskeluoikeusJakso pidennettyPäättymispäiväjakso = new OpiskeluoikeusJakso(period.getBegin(), OpiskeluoikeudenTila.lasna);
+            pidennettyPäättymispäiväjakso.setOpintojenRahoitus(new KoodistoViite<>(OpintojenRahoitus.K6));
+            tila.addOpiskeluoikeusJakso(pidennettyPäättymispäiväjakso);
+            
+            if (period.getEnd() != null) {
+              tila.addOpiskeluoikeusJakso(new OpiskeluoikeusJakso(period.getEnd(), OpiskeluoikeudenTila.lasna));
+            }
           break;
         }
       }
