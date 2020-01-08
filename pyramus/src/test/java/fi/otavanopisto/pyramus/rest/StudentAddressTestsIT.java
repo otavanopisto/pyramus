d7756850035e1478a11bc80e877a127b3596767a
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2020-01-08 13:59:41 +0200

    Tests for updating student contact info

--- pyramus/src/test/java/fi/otavanopisto/pyramus/rest/StudentAddressTestsIT.java
+++ pyramus/src/test/java/fi/otavanopisto/pyramus/rest/StudentAddressTestsIT.java
@@ -76,6 +76,53 @@ public class StudentAddressTestsIT extends AbstractRESTServiceTest {
   }  
 
   @Test
+  public void testUpdateStudentAddress() {
+    Address address = new Address(null, 1l, Boolean.FALSE, "Caleb Great", "24916 Nicole Land", "59903-2455", "Porthaven", "Uruguay");
+    
+    Response response = given().headers(getAuthHeaders())
+      .contentType("application/json")
+      .body(address)
+      .post("/students/students/{ID}/addresses", TEST_STUDENT_ID);
+
+    response.then()
+      .statusCode(200)
+      .body("id", not(is((Long) null)))
+      .body("name", is(address.getName()))
+      .body("streetAddress", is(address.getStreetAddress()))
+      .body("postalCode", is(address.getPostalCode()))
+      .body("city", is(address.getCity()))
+      .body("country", is(address.getCountry()))
+      .body("contactTypeId", is(address.getContactTypeId().intValue()))
+      .body("defaultAddress", is( address.getDefaultAddress()));
+    
+    long id = response.body().jsonPath().getLong("id");
+    try {
+      Address updatedAddress = new Address(id, 1l, Boolean.FALSE, "Caleb Great", "090-Mudflap", "1919-44", "Salamander", "Papua New-Guinea");
+    
+      given().headers(getAuthHeaders())
+        .contentType("application/json")
+        .body(updatedAddress)
+        .put("/students/students/{ID}/addresses/{ADDRESSID}", TEST_STUDENT_ID, id)
+        .then()
+        .statusCode(200)
+        .body("id", not(is((Long) null)))
+        .body("name", is(updatedAddress.getName()))
+        .body("streetAddress", is(updatedAddress.getStreetAddress()))
+        .body("postalCode", is(updatedAddress.getPostalCode()))
+        .body("city", is(updatedAddress.getCity()))
+        .body("country", is(updatedAddress.getCountry()))
+        .body("contactTypeId", is(updatedAddress.getContactTypeId().intValue()))
+        .body("defaultAddress", is(updatedAddress.getDefaultAddress()));
+      
+    } finally {
+      given().headers(getAuthHeaders())
+        .delete("/students/students/{STUDENTID}/addresses/{ID}", TEST_STUDENT_ID, id)
+        .then()
+        .statusCode(204);
+    }
+  }
+  
+  @Test
   public void testDeleteStudentAddress() {
     Address address = new Address(null, 1l, Boolean.FALSE, "Caleb Great", "24916 Nicole Land", "59903-2455", "Porthaven", "Uruguay");
     
--- pyramus/src/test/java/fi/otavanopisto/pyramus/rest/StudentPhoneNumberTestsIT.java
+++ pyramus/src/test/java/fi/otavanopisto/pyramus/rest/StudentPhoneNumberTestsIT.java
@@ -65,6 +65,44 @@ public class StudentPhoneNumberTestsIT extends AbstractRESTServiceTest {
   }  
 
   @Test
+  public void testUpdateStudentPhoneNumber() {
+    PhoneNumber phoneNumber = new PhoneNumber(null, 1l, Boolean.FALSE, "(123) 1234 567");
+    
+    Response response = given().headers(getAuthHeaders())
+      .contentType("application/json")
+      .body(phoneNumber)
+      .post("/students/students/{ID}/phoneNumbers", TEST_STUDENT_ID);
+
+    response.then()
+      .statusCode(200)
+      .body("id", not(is((Long) null)))
+      .body("number", is(phoneNumber.getNumber()))
+      .body("contactTypeId", is(phoneNumber.getContactTypeId().intValue()))
+      .body("defaultNumber", is( phoneNumber.getDefaultNumber()));
+      
+    long id = response.body().jsonPath().getLong("id");
+    try {
+      PhoneNumber updatedPhoneNumber = new PhoneNumber(id, 1l, Boolean.FALSE, "(888) 432 1098");
+        
+      given().headers(getAuthHeaders())
+        .contentType("application/json")
+        .body(updatedPhoneNumber)
+        .put("/students/students/{ID}/phoneNumbers/{PHONENUMBERID}", TEST_STUDENT_ID, id)
+        .then()
+        .statusCode(200)
+        .body("id", not(is((Long) null)))
+        .body("number", is(updatedPhoneNumber.getNumber()))
+        .body("contactTypeId", is(updatedPhoneNumber.getContactTypeId().intValue()))
+        .body("defaultNumber", is(updatedPhoneNumber.getDefaultNumber()));
+    } finally {
+      given().headers(getAuthHeaders())
+        .delete("/students/students/{STUDENTID}/phoneNumbers/{ID}", TEST_STUDENT_ID, id)
+        .then()
+        .statusCode(204);
+    }
+  }
+  
+  @Test
   public void testDeleteStudentPhoneNumber() {
     PhoneNumber phoneNumber = new PhoneNumber(null, 1l, Boolean.FALSE, "(123) 12 234 5678");
     
