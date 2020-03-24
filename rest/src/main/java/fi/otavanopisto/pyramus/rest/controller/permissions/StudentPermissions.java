107ed99ed8890fb30f6150dcf9437d0ad326587f
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2020-03-24 11:56:31 +0200

    Added trusted system for listing the study periods

--- rest/src/main/java/fi/otavanopisto/pyramus/rest/controller/permissions/StudentPermissions.java
+++ rest/src/main/java/fi/otavanopisto/pyramus/rest/controller/permissions/StudentPermissions.java
@@ -192,11 +192,11 @@ public class StudentPermissions extends AbstractPyramusPermissionCollection impl
   public static final String CREATE_STUDENTSTUDYPERIOD = "CREATE_STUDENTSTUDYPERIOD";
   
   @Scope (PermissionScope.ENVIRONMENT)
-  @DefaultPermissionRoles ({ ADMINISTRATOR, MANAGER, STUDY_PROGRAMME_LEADER, STUDY_GUIDER })
+  @DefaultPermissionRoles ({ ADMINISTRATOR, MANAGER, STUDY_PROGRAMME_LEADER, STUDY_GUIDER, TRUSTED_SYSTEM })
   public static final String LIST_STUDENTSTUDYPERIODS = "LIST_STUDENTSTUDYPERIODS";
   
   @Scope (PermissionScope.ENVIRONMENT)
-  @DefaultPermissionRoles ({ ADMINISTRATOR, MANAGER, STUDY_PROGRAMME_LEADER, STUDY_GUIDER })
+  @DefaultPermissionRoles ({ ADMINISTRATOR, MANAGER, STUDY_PROGRAMME_LEADER, STUDY_GUIDER, TRUSTED_SYSTEM })
   public static final String FIND_STUDENTSTUDYPERIOD = "FIND_STUDENTSTUDYPERIOD";
   
   @Scope (PermissionScope.ENVIRONMENT)
