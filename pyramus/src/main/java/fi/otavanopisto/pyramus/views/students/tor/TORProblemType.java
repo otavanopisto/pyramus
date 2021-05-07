e48042ef97c53dbffcac63f640fa0ec41c05514b
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2021-05-07 13:17:03 +0300

    KA

---
+++ pyramus/src/main/java/fi/otavanopisto/pyramus/views/students/tor/TORProblemType.java
@@ -0,0 +1,15 @@
+package fi.otavanopisto.pyramus.views.students.tor;
+
+public enum TORProblemType {
+  
+  /**
+   * Course length units from two or more sources didn't match
+   */
+  INCOMPATIBLE_LENGTHUNITS, 
+  
+  /**
+   * Length unit could not be resolved
+   */
+  UNRESOLVABLE_LENGTHUNIT
+
+}
