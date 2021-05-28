579be8e00816f56ec4e11224749f5a3d0b835535
Pasi Kukkonen <pasi.kukkonen@otavanopisto.fi>
2021-05-28 10:05:22 +0300

    - resolves #1254

---
+++ framework/src/main/java/fi/otavanopisto/pyramus/schedulers/ClientApplicationTokenCleaner.java
@@ -0,0 +1,66 @@
+package fi.otavanopisto.pyramus.schedulers;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+
+import fi.otavanopisto.pyramus.dao.clientapplications.ClientApplicationAccessTokenDAO;
+import fi.otavanopisto.pyramus.dao.clientapplications.ClientApplicationAuthorizationCodeDAO;
+import fi.otavanopisto.pyramus.domainmodel.clientapplications.ClientApplicationAccessToken;
+import fi.otavanopisto.pyramus.domainmodel.clientapplications.ClientApplicationAuthorizationCode;
+import fi.otavanopisto.pyramus.domainmodel.users.Role;
+
+@Singleton
+@Startup
+public class ClientApplicationTokenCleaner {
+
+  @Inject
+  private Logger logger;
+  
+  @Inject
+  private ClientApplicationAccessTokenDAO clientApplicationAccessTokenDAO;
+  
+  @Inject
+  private ClientApplicationAuthorizationCodeDAO clientApplicationAuthorizationCodeDAO;
+  
+  @Schedule(dayOfWeek = "*", hour="6", persistent = false)
+  private void removeExpiredTokens() {
+    int removed = 0;
+    Calendar calendar = new GregorianCalendar();
+    calendar.setTime(new Date());
+    calendar.add(Calendar.DATE, -1);
+    long threshold = calendar.getTimeInMillis() / 1000;
+    
+    long tokenCount = clientApplicationAccessTokenDAO.count(); 
+    if (tokenCount > 10000) {
+      logger.severe(String.format("Too many client application access tokens to clean (%d)", tokenCount));
+      return;
+    }
+    
+    List<ClientApplicationAccessToken> tokens = clientApplicationAccessTokenDAO.listAll();
+    for (ClientApplicationAccessToken token : tokens) {
+      ClientApplicationAuthorizationCode authCode = token.getClientApplicationAuthorizationCode();
+      if (authCode.getUser().getRole() == Role.TRUSTED_SYSTEM) {
+        continue;
+      }
+      if (token.getExpires() < threshold) {
+        clientApplicationAccessTokenDAO.delete(token);
+        clientApplicationAuthorizationCodeDAO.delete(authCode);
+        removed++;
+      }
+    }
+
+    if (removed > 0) {
+      logger.info(String.format("Removed %d expired client application access tokens", removed));
+    }
+    
+  }
+
+}
