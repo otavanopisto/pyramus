95463042b92933b67c159c4b3a9fb7a1685acae9
aviljakaine <antti.viljakainen@otavanopisto.fi>
2021-09-07 15:02:43 +0300

    TOR moved to framework for accessibility

--- rest-model/src/main/java/fi/otavanopisto/pyramus/rest/model/StudentCourseStats.java
+++ rest-model/src/main/java/fi/otavanopisto/pyramus/rest/model/StudentCourseStats.java
@@ -1,15 +1,23 @@
 package fi.otavanopisto.pyramus.rest.model;
 
 public class StudentCourseStats {
-	
-	public int getNumberCompletedCourses() {
-		return numberCompletedCourses;
-	}
 
-	public void setNumberCompletedCourses(int numberCompletedCourses) {
-		this.numberCompletedCourses = numberCompletedCourses;
-	}
+  public int getNumberCompletedCourses() {
+    return numberCompletedCourses;
+  }
 
-	int numberCompletedCourses;
+  public void setNumberCompletedCourses(int numberCompletedCourses) {
+    this.numberCompletedCourses = numberCompletedCourses;
+  }
 
+  public double getNumberCreditPoints() {
+    return numberCreditPoints;
+  }
+
+  public void setNumberCreditPoints(double numberCreditPoints) {
+    this.numberCreditPoints = numberCreditPoints;
+  }
+
+  private int numberCompletedCourses;
+  private double numberCreditPoints;
 }
