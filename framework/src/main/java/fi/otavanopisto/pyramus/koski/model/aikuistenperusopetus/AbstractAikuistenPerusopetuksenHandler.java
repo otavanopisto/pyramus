168c4f081a8df30211fd761306827890d0bd6dbe
aviljakaine <antti.viljakainen@otavanopisto.fi>
2022-02-14 14:38:05 +0200

    Cleanup of deprecated fields

--- framework/src/main/java/fi/otavanopisto/pyramus/koski/model/aikuistenperusopetus/AbstractAikuistenPerusopetuksenHandler.java
+++ framework/src/main/java/fi/otavanopisto/pyramus/koski/model/aikuistenperusopetus/AbstractAikuistenPerusopetuksenHandler.java
@@ -1,9 +1,6 @@
 package fi.otavanopisto.pyramus.koski.model.aikuistenperusopetus;
 
-import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 import javax.inject.Inject;
 
@@ -35,23 +32,9 @@ public abstract class AbstractAikuistenPerusopetuksenHandler extends KoskiStuden
   protected AikuistenPerusopetuksenOpiskeluoikeudenLisatiedot getLisatiedot(Student student) {
     AikuistenPerusopetuksenOpiskeluoikeudenLisatiedot lisatiedot = new AikuistenPerusopetuksenOpiskeluoikeudenLisatiedot();
     
-    lisatiedot.setVuosiluokkiinSitoutumatonOpetus(true);
-    
     List<StudentLodgingPeriod> lodgingPeriods = lodgingPeriodDAO.listByStudent(student);
     for (StudentLodgingPeriod lodgingPeriod : lodgingPeriods) {
       lisatiedot.addSisaoppilaitosmainenMajoitus(new Majoitusjakso(lodgingPeriod.getBegin(), lodgingPeriod.getEnd()));
-    }
-    
-    if (!lodgingPeriods.isEmpty()) {
-      Date minBeginDate = lodgingPeriods.stream().map(StudentLodgingPeriod::getBegin).filter(Objects::nonNull)
-          .min(Comparator.nullsLast(Date::compareTo)).orElse(null);
-      
-      if (minBeginDate != null) {
-        Date maxEndDate = lodgingPeriods.stream().map(StudentLodgingPeriod::getEnd).filter(Objects::nonNull)
-            .max(Comparator.nullsLast(Date::compareTo)).orElse(null);
-        
-        lisatiedot.setOikeusMaksuttomaanAsuntolapaikkaan(new Majoitusjakso(minBeginDate, maxEndDate));
-      }
     }
     
     List<StudentStudyPeriod> studyPeriods = studentStudyPeriodDAO.listByStudent(student);
--- framework/src/main/java/fi/otavanopisto/pyramus/koski/model/lukio/AbstractKoskiLukioStudentHandler.java
+++ framework/src/main/java/fi/otavanopisto/pyramus/koski/model/lukio/AbstractKoskiLukioStudentHandler.java
@@ -5,14 +5,11 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.apache.commons.lang3.StringUtils;
-
 import fi.otavanopisto.pyramus.dao.students.StudentStudyPeriodDAO;
 import fi.otavanopisto.pyramus.domainmodel.students.Student;
 import fi.otavanopisto.pyramus.domainmodel.students.StudentLodgingPeriod;
 import fi.otavanopisto.pyramus.domainmodel.students.StudentStudyPeriod;
 import fi.otavanopisto.pyramus.domainmodel.students.StudentStudyPeriodType;
-import fi.otavanopisto.pyramus.koski.KoskiConsts;
 import fi.otavanopisto.pyramus.koski.KoskiStudentHandler;
 import fi.otavanopisto.pyramus.koski.model.Majoitusjakso;
 
@@ -21,9 +18,6 @@ import fi.otavanopisto.pyramus.koski.model.Majoitusjakso;
  */
 public abstract class AbstractKoskiLukioStudentHandler extends KoskiStudentHandler {
 
-  public static final String USERVARIABLE_UNDER18START = KoskiConsts.UserVariables.STARTED_UNDER18;
-  public static final String USERVARIABLE_UNDER18STARTREASON = KoskiConsts.UserVariables.UNDER18_STARTREASON;
-
   @Inject
   private StudentStudyPeriodDAO studentStudyPeriodDAO;
   
@@ -31,18 +25,9 @@ public abstract class AbstractKoskiLukioStudentHandler extends KoskiStudentHandl
     List<StudentStudyPeriod> studyPeriods = studentStudyPeriodDAO.listByStudent(student);
     boolean pidennettyPaattymispaiva = studyPeriods.stream().anyMatch(studyPeriod -> studyPeriod.getPeriodType() == StudentStudyPeriodType.PROLONGED_STUDYENDDATE);
     boolean ulkomainenVaihtoopiskelija = false;
-    boolean yksityisopiskelija = settings.isYksityisopiskelija(student.getStudyProgramme().getId());
-    boolean oikeusMaksuttomaanAsuntolapaikkaan = settings.isFreeLodging(student.getStudyProgramme().getId());
     LukionOpiskeluoikeudenLisatiedot lisatiedot = new LukionOpiskeluoikeudenLisatiedot(
-        pidennettyPaattymispaiva, ulkomainenVaihtoopiskelija, yksityisopiskelija, oikeusMaksuttomaanAsuntolapaikkaan);
+        pidennettyPaattymispaiva, ulkomainenVaihtoopiskelija);
 
-    if (StringUtils.equals(userVariableDAO.findByUserAndKey(student, USERVARIABLE_UNDER18START), "1")) {
-      String under18startReason = userVariableDAO.findByUserAndKey(student, USERVARIABLE_UNDER18STARTREASON);
-      if (StringUtils.isNotBlank(under18startReason)) {
-        lisatiedot.setAlle18vuotiaanAikuistenLukiokoulutuksenAloittamisenSyy(kuvaus(under18startReason));
-      }
-    }
-    
     List<StudentLodgingPeriod> lodgingPeriods = lodgingPeriodDAO.listByStudent(student);
     for (StudentLodgingPeriod lodgingPeriod : lodgingPeriods) {
       Majoitusjakso jakso = new Majoitusjakso(lodgingPeriod.getBegin(), lodgingPeriod.getEnd());
