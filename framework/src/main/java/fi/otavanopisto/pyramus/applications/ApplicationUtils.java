015e3983c6344f4d51cfafaa2d4960afdb1f2200
Pasi Kukkonen <pasi.kukkonen@otavanopisto.fi>
2022-05-04 15:18:50 +0300

    - moar NPE fixes

--- framework/src/main/java/fi/otavanopisto/pyramus/applications/ApplicationUtils.java
+++ framework/src/main/java/fi/otavanopisto/pyramus/applications/ApplicationUtils.java
@@ -265,19 +265,25 @@ public class ApplicationUtils {
   }
   
   public static String genderUiValue(String value) {
-    switch (value) {
-    case "mies":
-      return "Mies";
-    case "nainen":
-      return "Nainen";
-    case "muu":
-      return "Muu";
-    default:
-      return null;
+    if (value != null) {
+      switch (value) {
+      case "mies":
+        return "Mies";
+      case "nainen":
+        return "Nainen";
+      case "muu":
+        return "Muu";
+      default:
+        return null;
+      }
     }
+    return null;
   }
   
   public static Curriculum resolveCurriculum(String curriculumValue) {
+    if (StringUtils.isEmpty(curriculumValue)) {
+      return null;
+    }
     CurriculumDAO curriculumDAO = DAOFactory.getInstance().getCurriculumDAO();
     switch (curriculumValue) {
     case "ops2016":
@@ -1118,38 +1124,41 @@ public class ApplicationUtils {
   }
 
   public static String sourceUiValue(String value) {
-    switch (value) {
-    case "tuttu":
-      return "Ennestään tuttu";
-    case "google":
-      return "Google";
-    case "facebook":
-      return "Facebook";
-    case "instagram":
-      return "Instagram";
-    case "sanomalehti":
-      return "Sanomalehti";
-    case "tienvarsimainos":
-      return "Tienvarsimainos";
-    case "valotaulumainos":
-      return "Valotaulumainos";
-    case "elokuva":
-      return "Elokuva- tai TV-mainos";
-    case "tuttava":
-      return "Kuulin kaverilta, tuttavalta, tms.";
-    case "opot":
-      return "Opot";
-    case "messut":
-      return "Messut";
-    case "te-toimisto":
-      return "TE-toimisto";
-    case "nuorisotyo":
-      return "Nuorisotyö";
-    case "muu":
-      return "Muu";
-    default:
-      return null;
+    if (value != null) {
+      switch (value) {
+      case "tuttu":
+        return "Ennestään tuttu";
+      case "google":
+        return "Google";
+      case "facebook":
+        return "Facebook";
+      case "instagram":
+        return "Instagram";
+      case "sanomalehti":
+        return "Sanomalehti";
+      case "tienvarsimainos":
+        return "Tienvarsimainos";
+      case "valotaulumainos":
+        return "Valotaulumainos";
+      case "elokuva":
+        return "Elokuva- tai TV-mainos";
+      case "tuttava":
+        return "Kuulin kaverilta, tuttavalta, tms.";
+      case "opot":
+        return "Opot";
+      case "messut":
+        return "Messut";
+      case "te-toimisto":
+        return "TE-toimisto";
+      case "nuorisotyo":
+        return "Nuorisotyö";
+      case "muu":
+        return "Muu";
+      default:
+        return null;
+      }
     }
+    return null;
   }
   
   /**
