bc2db0493dffaec2af8c91ae74bea8025f690aa8
aviljakaine <antti.viljakainen@otavanopisto.fi>
2022-09-29 14:49:55 +0300

    Fixed deserialization issues

--- framework/src/main/java/fi/otavanopisto/pyramus/koski/koodisto/OpintojenLaajuusYksikko.java
+++ framework/src/main/java/fi/otavanopisto/pyramus/koski/koodisto/OpintojenLaajuusYksikko.java
@@ -1,26 +1,48 @@
 package fi.otavanopisto.pyramus.koski.koodisto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import fi.otavanopisto.pyramus.koski.KoodistoEnum;
 
 @KoodistoEnum("opintojenlaajuusyksikko")
 public enum OpintojenLaajuusYksikko {
 
-  ov (1),
-  op (2),
-  vuosiviikkotuntia (3),
-  kurssia (4),
-  tuntia (5),
-  osp (6),
-  vuotta (7);
+  ov ("1"),
+  op ("2"),
+  vuosiviikkotuntia ("3"),
+  kurssia ("4"),
+  tuntia ("5"),
+  osp ("6"),
+  vuotta ("7");
   
-  OpintojenLaajuusYksikko(int value) {
+  OpintojenLaajuusYksikko(String value) {
     this.value = value;
   }
   
   @Override
   public String toString() {
-    return String.valueOf(value);
+    return value;
   }
   
-  private int value;
+  public String getValue() {
+    return value;
+  }
+  
+  public static OpintojenLaajuusYksikko get(String value) {
+    return lookup.get(value);
+  }
+  
+  public static OpintojenLaajuusYksikko reverseLookup(String value) {
+    return get(value);
+  }
+  
+  private String value;
+  private static Map<String, OpintojenLaajuusYksikko> lookup = new HashMap<>();
+
+  static {
+    for (OpintojenLaajuusYksikko v : values()) {
+      lookup.put(v.getValue(), v);
+    }
+  }
 }
--- pyramus/src/test/java/fi/otavanopisto/pyramus/koski/lukio2019/KoskiLukio2019JSONTestsIT.java
+++ pyramus/src/test/java/fi/otavanopisto/pyramus/koski/lukio2019/KoskiLukio2019JSONTestsIT.java
@@ -15,10 +15,20 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import fi.otavanopisto.pyramus.koski.AbstractKoskiTest;
+import fi.otavanopisto.pyramus.koski.koodisto.Kielivalikoima;
+import fi.otavanopisto.pyramus.koski.koodisto.KoskiOppiaineetYleissivistava;
+import fi.otavanopisto.pyramus.koski.koodisto.ModuuliKoodistoLOPS2021;
+import fi.otavanopisto.pyramus.koski.koodisto.OpintojenLaajuusYksikko;
 import fi.otavanopisto.pyramus.koski.model.HenkiloTiedotJaOID;
 import fi.otavanopisto.pyramus.koski.model.Oppija;
 import fi.otavanopisto.pyramus.koski.model.lukio.LukionOpiskeluoikeus;
 import fi.otavanopisto.pyramus.koski.model.lukio.LukionSuoritus;
+import fi.otavanopisto.pyramus.koski.model.lukio.ops2019.LukionOpintojaksonSuoritus2019;
+import fi.otavanopisto.pyramus.koski.model.lukio.ops2019.LukionOpintojaksonTunnisteMuuModuuli2019;
+import fi.otavanopisto.pyramus.koski.model.lukio.ops2019.LukionOpintojaksonTunnisteVierasKieli2019;
+import fi.otavanopisto.pyramus.koski.model.lukio.ops2019.LukionOppiaineenSuoritus2019;
+import fi.otavanopisto.pyramus.koski.model.lukio.ops2019.LukionOppiaineenSuoritusMuuValtakunnallinen2019;
+import fi.otavanopisto.pyramus.koski.model.lukio.ops2019.LukionOppiaineenSuoritusVierasKieli2019;
 import fi.otavanopisto.pyramus.koski.model.lukio.ops2019.LukionOppimaaranSuoritus2019;
 
 public class KoskiLukio2019JSONTestsIT extends AbstractKoskiTest {
@@ -81,6 +91,63 @@ public class KoskiLukio2019JSONTestsIT extends AbstractKoskiTest {
       LukionSuoritus lukionSuoritus = opiskeluoikeus.getSuoritukset().iterator().next();
       assertTrue(String.format("Deserialization returned wrong type %s, expected %s", lukionSuoritus.getClass().getSimpleName(), LukionOppimaaranSuoritus2019.class.getSimpleName()), 
           lukionSuoritus instanceof LukionOppimaaranSuoritus2019);
+      
+      LukionOppimaaranSuoritus2019 lukionOppimaaranSuoritus2019 = (LukionOppimaaranSuoritus2019) lukionSuoritus;
+      
+      assertEquals(4, lukionOppimaaranSuoritus2019.getOsasuoritukset().size());
+      
+      // KE
+      
+      LukionOppiaineenSuoritus2019 kemia = lukionOppimaaranSuoritus2019.getOsasuoritukset().stream()
+          .filter(lo2019 -> lo2019 instanceof LukionOppiaineenSuoritus2019)
+          .map(lo2019 -> (LukionOppiaineenSuoritus2019) lo2019)
+          .filter(los2019 -> los2019.getKoulutusmoduuli() instanceof LukionOppiaineenSuoritusMuuValtakunnallinen2019)
+          .filter(los2019 -> ((LukionOppiaineenSuoritusMuuValtakunnallinen2019) los2019.getKoulutusmoduuli()).getTunniste().getValue() == KoskiOppiaineetYleissivistava.KE)
+          .findFirst()
+          .orElse(null);
+
+      assertNotNull("KE not found", kemia);
+      
+      LukionOpintojaksonSuoritus2019 ke1 = kemia.getOsasuoritukset().get(0);
+      
+      assertTrue(ke1.getKoulutusmoduuli() instanceof LukionOpintojaksonTunnisteMuuModuuli2019);
+      
+      LukionOpintojaksonTunnisteMuuModuuli2019 moduuli = (LukionOpintojaksonTunnisteMuuModuuli2019) ke1.getKoulutusmoduuli();
+      
+      assertNotNull(moduuli.getTunniste());
+      assertEquals(ModuuliKoodistoLOPS2021.KE1, moduuli.getTunniste().getValue());
+      assertTrue(moduuli.isPakollinen());
+      assertEquals(10, moduuli.getLaajuus().getArvo());
+      assertEquals(OpintojenLaajuusYksikko.op, moduuli.getLaajuus().getYksikko().getValue());
+      
+      // ENA
+
+      LukionOppiaineenSuoritus2019 ena = lukionOppimaaranSuoritus2019.getOsasuoritukset().stream()
+          .filter(lo2019 -> lo2019 instanceof LukionOppiaineenSuoritus2019)
+          .map(lo2019 -> (LukionOppiaineenSuoritus2019) lo2019)
+          .filter(los2019 -> los2019.getKoulutusmoduuli() instanceof LukionOppiaineenSuoritusVierasKieli2019)
+          .filter(los2019 -> ((LukionOppiaineenSuoritusVierasKieli2019) los2019.getKoulutusmoduuli()).getTunniste().getValue() == KoskiOppiaineetYleissivistava.A)
+          .findFirst()
+          .orElse(null);
+
+      assertNotNull("ENA not found", ena);
+
+      LukionOppiaineenSuoritusVierasKieli2019 ena_ainetunniste = (LukionOppiaineenSuoritusVierasKieli2019) ena.getKoulutusmoduuli();
+
+      assertNotNull(ena_ainetunniste.getKieli());
+      assertEquals(Kielivalikoima.EN, ena_ainetunniste.getKieli().getValue());
+      
+      LukionOpintojaksonSuoritus2019 ena1 = ena.getOsasuoritukset().get(0);
+      
+      assertTrue(ena1.getKoulutusmoduuli() instanceof LukionOpintojaksonTunnisteVierasKieli2019);
+      
+      LukionOpintojaksonTunnisteVierasKieli2019 ena1_tunniste = (LukionOpintojaksonTunnisteVierasKieli2019) ena1.getKoulutusmoduuli();
+      
+      assertNotNull(ena1_tunniste.getTunniste());
+      assertEquals(ModuuliKoodistoLOPS2021.ENA1, ena1_tunniste.getTunniste().getValue());
+      assertTrue(ena1_tunniste.isPakollinen());
+      assertEquals(38, ena1_tunniste.getLaajuus().getArvo());
+      assertEquals(OpintojenLaajuusYksikko.op, ena1_tunniste.getLaajuus().getYksikko().getValue());
       
       // TODO: check all the rest million properties
     }
