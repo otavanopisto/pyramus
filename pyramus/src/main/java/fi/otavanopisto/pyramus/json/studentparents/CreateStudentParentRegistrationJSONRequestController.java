9aded0411b2995c4130081b9592cd28c730f0f73
aviljakaine <antti.viljakainen@otavanopisto.fi>
2023-11-30 13:38:45 +0200

    Send mail when creating an parent invitation

--- pyramus/src/main/java/fi/otavanopisto/pyramus/json/studentparents/CreateStudentParentRegistrationJSONRequestController.java
+++ pyramus/src/main/java/fi/otavanopisto/pyramus/json/studentparents/CreateStudentParentRegistrationJSONRequestController.java
@@ -2,16 +2,24 @@ package fi.otavanopisto.pyramus.json.studentparents;
 
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.IOUtils;
+
+import fi.internetix.smvc.SmvcRuntimeException;
 import fi.internetix.smvc.controllers.JSONRequestContext;
+import fi.otavanopisto.pyramus.applications.ApplicationUtils;
 import fi.otavanopisto.pyramus.dao.DAOFactory;
 import fi.otavanopisto.pyramus.dao.students.StudentDAO;
 import fi.otavanopisto.pyramus.dao.users.StaffMemberDAO;
 import fi.otavanopisto.pyramus.dao.users.StudentParentRegistrationDAO;
 import fi.otavanopisto.pyramus.domainmodel.students.Student;
 import fi.otavanopisto.pyramus.domainmodel.users.StaffMember;
+import fi.otavanopisto.pyramus.domainmodel.users.StudentParentRegistration;
 import fi.otavanopisto.pyramus.framework.JSONRequestController;
 import fi.otavanopisto.pyramus.framework.UserRole;
 import fi.otavanopisto.pyramus.framework.UserUtils;
+import fi.otavanopisto.pyramus.mailer.Mailer;
 
 public class CreateStudentParentRegistrationJSONRequestController extends JSONRequestController {
 
@@ -43,10 +51,33 @@ public class CreateStudentParentRegistrationJSONRequestController extends JSONRe
     String lastName = requestContext.getString("lastName");
     String email = requestContext.getString("email");
 
-    
-    // TODO move to controller and send email about the thing
     String hash = UUID.randomUUID().toString();
-    studentParentRegistrationDAO.create(firstName, lastName, email, student, hash);
+    StudentParentRegistration guardian = studentParentRegistrationDAO.create(firstName, lastName, email, student, hash);
+
+    // Send mail
+    
+    try {
+      HttpServletRequest request = requestContext.getRequest();
+      String guardianEmailContent = IOUtils.toString(request.getServletContext().getResourceAsStream(
+          "/templates/applications/mails/mail-credentials-guardian-create.html"), "UTF-8");
+      
+      StringBuffer guardianCreateCredentialsLink = new StringBuffer(ApplicationUtils.getRequestURIRoot(request));
+      guardianCreateCredentialsLink.append("/parentregister.page?c=");
+      guardianCreateCredentialsLink.append(guardian.getHash());
+      
+      String subject = "Muikku-oppimisympäristön tunnukset";
+      String content = String.format(guardianEmailContent, guardian.getFirstName(), guardianCreateCredentialsLink.toString());
+      
+      Mailer.sendMail(
+          Mailer.JNDI_APPLICATION,
+          Mailer.HTML,
+          null,
+          guardian.getEmail(),
+          subject,
+          content);
+    } catch (Exception ex) {
+      throw new SmvcRuntimeException(ex);
+    }
     
     requestContext.setRedirectURL(requestContext.getReferer(true));
   }
