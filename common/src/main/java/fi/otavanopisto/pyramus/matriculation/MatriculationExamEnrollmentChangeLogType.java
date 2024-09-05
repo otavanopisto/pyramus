36a7792d5819c7069ee8f3975a54b476bbff63f5
aviljakaine <antti.viljakainen@otavanopisto.fi>
2024-09-05 12:20:53 +0300

    Review changes

--- common/src/main/java/fi/otavanopisto/pyramus/matriculation/MatriculationExamEnrollmentChangeLogType.java
+++ common/src/main/java/fi/otavanopisto/pyramus/matriculation/MatriculationExamEnrollmentChangeLogType.java
@@ -1,11 +1,24 @@
 package fi.otavanopisto.pyramus.matriculation;
 
 public enum MatriculationExamEnrollmentChangeLogType {
-  // Enrollment was created
+  /**
+   * Enrollment was created
+   */
   ENROLLMENT_CREATED,
-  // Enrollment was updated
+  
+  /**
+   * Enrollment was updated. Use this ideally only when the fields have changed.
+   * The state may or may not change within the same log entry. I.e. if both
+   * the fields and state are changed, only add one ENROLLMENT_UPDATED log entry
+   * instead of ENROLLMENT_UPDATED and STATE_CHANGED.
+   */
   ENROLLMENT_UPDATED,
-  // Enrollment's state was changed f.ex. study guider approves the enrollment.
-  // When using this option, fill the newState field for the log entry.
+
+  /**
+   * Enrollment's state was changed f.ex. study guider approves the enrollment.
+   * Use this log type when only the state has changed.
+   * 
+   * When using this option, fill the newState field for the log entry.
+   */
   STATE_CHANGED
 }
