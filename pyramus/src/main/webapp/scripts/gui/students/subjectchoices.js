a1ebb2887196e9e04c6a63230a58989b34c46bdc
aviljakaine <antti.viljakainen@otavanopisto.fi>
2024-11-27 15:32:51 +0200

    KIA/KIB3 subject selections

--- pyramus/src/main/webapp/scripts/gui/students/subjectchoices.js
+++ pyramus/src/main/webapp/scripts/gui/students/subjectchoices.js
@@ -137,6 +137,8 @@ function updateTable(event) {
             selectController.addOption(selectHandlerInstance, "ARB3", "Arabia (ARB3)");
             selectController.addOption(selectHandlerInstance, "JPB3", "Japani (JPB3)");
             selectController.addOption(selectHandlerInstance, "KOB3", "Korea (KOB3)");
+            selectController.addOption(selectHandlerInstance, "KIA", "Kiinan kieli (KIA)");
+            selectController.addOption(selectHandlerInstance, "KIB3", "Kiinan kieli (KIB3)");
             selectController.addOption(selectHandlerInstance, "KXB3", "Muu kieli (KXB3)");
           }
         break;
