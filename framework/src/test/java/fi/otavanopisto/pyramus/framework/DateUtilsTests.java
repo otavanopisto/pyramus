bf44e5151576567d25dddb85dcc61603e9d4f7bc
aviljakaine <antti.viljakainen@otavanopisto.fi>
2025-05-14 11:36:47 +0300

    Fixed tests

--- framework/src/test/java/fi/otavanopisto/pyramus/framework/DateUtilsTests.java
+++ framework/src/test/java/fi/otavanopisto/pyramus/framework/DateUtilsTests.java
@@ -16,20 +16,20 @@ public class DateUtilsTests {
     cal.set(1990, 0, 1);
     Date first = cal.getTime();
 
-    assertEquals(1990, first.getYear());
+    assertEquals(1990, 1900 + first.getYear());
     assertEquals(0, first.getMonth());
     assertEquals(1, first.getDate());
 
     cal.set(2001, 1, 2);
     Date second = cal.getTime();
 
-    assertEquals(2001, second.getYear());
+    assertEquals(2001, 1900 + second.getYear());
     assertEquals(1, second.getMonth());
     assertEquals(2, second.getDate());
     
     Date max = DateUtils.max(first, second);
     
-    assertEquals(2001, max.getYear());
+    assertEquals(2001, 1900 + max.getYear());
     assertEquals(1, max.getMonth());
     assertEquals(2, max.getDate());
   }
