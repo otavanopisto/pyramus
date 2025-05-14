b357c06a861d6fc3dab70811c0a1362bccf9b441
aviljakaine <antti.viljakainen@otavanopisto.fi>
2025-05-14 12:54:11 +0300

    Added tab to matriculation exam view for eligible groups

--- framework/src/main/java/fi/otavanopisto/pyramus/PyramusConsts.java
+++ framework/src/main/java/fi/otavanopisto/pyramus/PyramusConsts.java
@@ -21,4 +21,8 @@ public class PyramusConsts {
   public static final String TIMEUNIT_OP = "op";
   public static final String TIMEUNIT_HOURS = "h";
   
+  public static class Matriculation {
+    public static final String USERVARIABLE_PERSONAL_EXAM_ENROLLMENT_EXPIRYDATE = "matriculation.examEnrollmentExpiryDate";
+    public static final String SETTING_ELIGIBLE_GROUPS = "matriculation.eligibleGroups";
+  }
 }
