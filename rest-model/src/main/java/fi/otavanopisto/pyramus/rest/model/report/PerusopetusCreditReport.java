5856d0608400d8cd786b990ac708c0b497fdbeef
Antti Viljakainen <antti.viljakainen@otavanopisto.fi>
2026-06-01 12:51:07 +0300

    Perusopetusraport improvements pt.1

--- rest-model/src/main/java/fi/otavanopisto/pyramus/rest/model/report/PerusopetusCreditReport.java
+++ rest-model/src/main/java/fi/otavanopisto/pyramus/rest/model/report/PerusopetusCreditReport.java
@@ -1,7 +1,9 @@
 package fi.otavanopisto.pyramus.rest.model.report;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PerusopetusCreditReport {
 
@@ -21,6 +23,63 @@ public class PerusopetusCreditReport {
     return rejectedCredits;
   }
 
+  public PerusopetusCreditReportSummary getSummary() {
+    return summary;
+  }
+
+  public class PerusopetusCreditReportSummary {
+    
+    public int getAcceptedCreditCount() {
+      return acceptedCreditCount;
+    }
+    
+    public void incrementAcceptedCreditCount() {
+      this.acceptedCreditCount++;
+    }
+    
+    public int getRejectedCreditCount() {
+      return rejectedCreditCount;
+    }
+    
+    public void incrementRejectedCreditCount() {
+      this.rejectedCreditCount++;
+    }
+
+    public Map<String, Integer> getAcceptedByLengthUnit() {
+      return acceptedByLengthUnit;
+    }
+
+    public void incrementAcceptedByLengthUnit(String acceptedLengthUnit) {
+      if (this.acceptedByLengthUnit.containsKey(acceptedLengthUnit)) {
+        Integer value = this.acceptedByLengthUnit.get(acceptedLengthUnit);
+        this.acceptedByLengthUnit.put(acceptedLengthUnit, value++);
+      } 
+      else {
+        this.acceptedByLengthUnit.put(acceptedLengthUnit, 1);
+      }
+    }
+
+    public Map<String, Integer> getRejectedByLengthUnit() {
+      return rejectedByLengthUnit;
+    }
+
+    public void incrementRejectedByLengthUnit(String rejectedLengthUnit) {
+      if (this.rejectedByLengthUnit.containsKey(rejectedLengthUnit)) {
+        Integer value = this.rejectedByLengthUnit.get(rejectedLengthUnit);
+        this.rejectedByLengthUnit.put(rejectedLengthUnit, value++);
+      } 
+      else {
+        this.rejectedByLengthUnit.put(rejectedLengthUnit, 1);
+      }
+    }
+
+    private int acceptedCreditCount;
+    private int rejectedCreditCount;
+    private Map<String, Integer> acceptedByLengthUnit = new HashMap<>();
+    private Map<String, Integer> rejectedByLengthUnit = new HashMap<>();
+  }
+  
+  private final PerusopetusCreditReportSummary summary = new PerusopetusCreditReportSummary();
   private final List<PerusopetusCredit> acceptedCredits = new ArrayList<>();
   private final List<PerusopetusCredit> rejectedCredits = new ArrayList<>();
 }
