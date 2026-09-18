be69584339a99a2122c3d2ad167b28d8490e7d5f
Antti Viljakainen <antti.viljakainen@otavanopisto.fi>
2026-09-18 12:57:05 +0300

    Apa kurssin laajuus

--- framework/src/main/java/fi/otavanopisto/pyramus/koski/model/apa/APAKurssinTunnistePaikallinen.java
+++ framework/src/main/java/fi/otavanopisto/pyramus/koski/model/apa/APAKurssinTunnistePaikallinen.java
@@ -3,6 +3,7 @@ package fi.otavanopisto.pyramus.koski.model.apa;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+import fi.otavanopisto.pyramus.koski.model.Laajuus;
 import fi.otavanopisto.pyramus.koski.model.PaikallinenKoodi;
 
 @JsonDeserialize(using = JsonDeserializer.None.class)
@@ -11,8 +12,9 @@ public class APAKurssinTunnistePaikallinen extends APAKurssinTunniste {
   public APAKurssinTunnistePaikallinen() {
   }
   
-  public APAKurssinTunnistePaikallinen(PaikallinenKoodi tunniste) {
+  public APAKurssinTunnistePaikallinen(PaikallinenKoodi tunniste, Laajuus laajuus) {
     this.tunniste = tunniste;
+    this.setLaajuus(laajuus);
   }
   
   public PaikallinenKoodi getTunniste() {
